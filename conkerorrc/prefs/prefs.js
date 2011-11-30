92ada7083e061aa84dae84e55dfc7b4aef2e0f32
Sharad Pratap <sh4r4d@gmail.com>
2011-11-30 23:03:10 +0530

    swank

--- conkerorrc/prefs/prefs.js
+++ conkerorrc/prefs/prefs.js
@@ -9,31 +9,18 @@
  * For more information, see http://www.mozilla.org/unix/customizing.html#prefs
  */
 
-user_pref("app.update.lastUpdateTime.addon-background-update-timer", 1322329995);
-user_pref("app.update.lastUpdateTime.background-update-timer", 1290846548);
-user_pref("app.update.lastUpdateTime.blocklist-background-update-timer", 1322330115);
-user_pref("app.update.lastUpdateTime.microsummary-generator-update-timer", 1322329995);
-user_pref("app.update.lastUpdateTime.search-engine-update-timer", 1322329995);
-user_pref("browser.active_color", "#EE0000");
-user_pref("browser.anchor_color", "#0000EE");
-user_pref("browser.cache.disk.capacity", 1048576);
-user_pref("browser.cache.disk.smart_size.first_run", false);
-user_pref("browser.cache.disk.smart_size_cached_value", 640000);
-user_pref("browser.display.background_color", "#FFFFFF");
-user_pref("browser.display.foreground_color", "#000000");
-user_pref("browser.display.use_system_colors", false);
-user_pref("browser.visited_color", "#551A8B");
-user_pref("extensions.blocklist.pingCountVersion", -1);
-user_pref("extensions.bootstrappedAddons", "{}");
-user_pref("extensions.databaseSchema", 3);
+user_pref("app.update.lastUpdateTime.addon-background-update-timer", 1322670152);
+user_pref("app.update.lastUpdateTime.background-update-timer", 1322670152);
+user_pref("app.update.lastUpdateTime.blocklist-background-update-timer", 1322670152);
+user_pref("browser.display.background_color", "#669933");
+user_pref("browser.display.foreground_color", "#003333");
+user_pref("browser.visited_color", "#660033");
 user_pref("extensions.enabledItems", "{6f9d85e0-794d-11dd-ad8b-0800200c9a66}:0.4,mozrepl@hyperstruct.net:2009122217");
-user_pref("extensions.installCache", "[]");
-user_pref("extensions.lastAppVersion", "0.9.3 (Debian-0.9.3+git110307-2ubuntu1)");
+user_pref("extensions.lastAppVersion", "0.9.3 (Debian-0.9.3+git101015-1ubuntu1~maverick1)");
 user_pref("extensions.mozrepl.autoStart", true);
 user_pref("extensions.newAddons", "{6f9d85e0-794d-11dd-ad8b-0800200c9a66},mozrepl@hyperstruct.net");
 user_pref("extensions.update.notifyUser", false);
-user_pref("idle.lastDailyNotification", 1289929857);
+user_pref("idle.lastDailyNotification", 1322670452);
 user_pref("network.cookie.prefsMigrated", true);
 user_pref("network.protocol-handler.external.mailto", true);
-user_pref("places.history.expiration.transient_current_max_pages", 108600);
 user_pref("spellchecker.dictionary", "en-US");
