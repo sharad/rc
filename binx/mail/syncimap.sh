26b551ef0b6588ddf9175fe866b02aad9e72b28c
Sharad Pratap <sh4r4d@gmail.com>
2012-12-31 16:55:23 +0530

    im

--- binx/mail/syncimap.sh
+++ binx/mail/syncimap.sh
@@ -25,7 +25,7 @@ function main() {
            nm-tool | egrep -q 'State:[[:space:]]+connected'; then
             if ! pgrep offlineimap 2>&1 > /dev/null ; then
                 if [  $interactive  ] ; then
-                    offlineimap -a ${account:-$OFFLINEIMAPACCOUNT} -u blinkenlights
+                    offlineimap -a ${account:-$OFFLINEIMAPACCOUNT}
                 else
                     timeout -s KILL 70 offlineimap -1 -u quiet -a ${account:-$OFFLINEIMAPACCOUNT}
                 fi
