b1c104dba13f5400c212461969eb54a259d1c9c4
Sharad Pratap <sh4r4d@gmail.com>
2013-02-13 19:42:00 +0530

    im

--- binx/utils/createnvrun.zsh
+++ binx/utils/createnvrun.zsh
@@ -7,9 +7,16 @@ file=$2
 
 if [ ! -e $1 ] ; then
 
-    if read -q \?" $file is missing do you want to create it? (y/n): " ; then
-        cp $template $file
-        general-editor $file
+    if [  -t 1 ] ; then
+        if read -q \?" $file is missing do you want to create it? (y/n): " ; then
+            cp $template $file
+            general-editor $file
+        fi
+    else
+        cat <<EOF>> ~/.var/run-later
+            cp $template $file
+            general-editor $file
+EOF
     fi
 
 fi
