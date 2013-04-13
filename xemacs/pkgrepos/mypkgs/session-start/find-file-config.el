b3b1a04cdae77c02df6fbcf5003e2be95c002738
Sharad Pratap <sh4r4d@gmail.com>
2013-04-13 16:38:50 +0530

    find file's wizard

--- xemacs/pkgrepos/mypkgs/session-start/find-file-config.el
+++ xemacs/pkgrepos/mypkgs/session-start/find-file-config.el
@@ -63,6 +63,8 @@ Bind this command to C-x C-f to get:
                                "Ido Find File: "
                                nil
                                jcl-ido-text))
+
+          ;; don't consider it.
           (let ((record (assoc file file-cache-alist)))
             (find-file
              (expand-file-name
@@ -72,19 +74,85 @@ Bind this command to C-x C-f to get:
                   (ido-completing-read (format "Find %s in dir: " file)
                                        (cdr record)
                                        nil
-                                       t))))))))
+                                       t)))))
+
+          )))
+
+(defvar *find-file-wizard-alist* nil "find-file-wizard-alist")
+
+(defmacro find-file-wizard-add (name ff-fun map magic-key hook ret-variable fun)
+  "hook where initial set could be defined\n
+"
+  `(let ((elt (cons ,name (list :ff-fun ',ff-fun :map ',map :key ',magic-key :hook ',hook :ret-variable ',ret-variable :fun ',fun))))
+     (push elt *find-file-wizard-alist*)))
 
 
+(find-file-wizard-add "ido1"
+                      ido-find-file
+                      ido-completion-map
+                      (kbd "C-x") ;; [(control ?f)]
+                      ido-setup-hook
+                      ido-text
+                      (progn
+                        (setq initial-string ido-text
+                              ido-text 'fallback
+                              ido-exit 'done)
+                        (exit-minibuffer)))
+
+;; (setq *find-file-wizard-alist* nil)
+
+;; *find-file-wizard-alist*
+
+(plist-get (cdr (assoc "ido1" *find-file-wizard-alist*)) :map)
+
+(defun find-file-wizard ()
+  (interactive)
+  (let ((wizard-alist *find-file-wizard-alist*)
+        (plist (cdar wizard-alist))
+        (file 'fallback)
+        initial-string)
+    (while (and
+            wizard-alist
+            (not (eq file 'fallback)))
+      (let ((plist (cdar wizard-alist)))
+        (setq file
+              (let ((:hook (cons (lambda ()
+                                   (define-key :map :key
+                                     (lambda (arg)
+                                       (interactive "P")
+                                       :fun
+                                       )))
+                                 ido-setup-hook)))
+                (:ff-fun initial-string))
+              wizard-alist (cdr wizard-alist))))
+    ;; (exit-minibuffer)
+    ;; file
+    ))
+
+;; (defun find-file-wizard ()
+;;   (let ((file))
+;;     (dolist (elt *find-file-wizard-alist*)
+;;       (setq file
+;;             (let ()
+;;               (funcall ))))))
+
+;; (defun find-file-wizard ()
+;;   (let ((file))
+;;     (loop for (elt1 elt2) on *find-file-wizard-alist*
+;;        do (let ()
+;;             ))))
 
 
+;; (loop for (a b) on '(1 2 3 4)
+;;    do (message "%s" (cons a b)))
 
-(progn
-  (let ((ido-setup-hook (cons (lambda ()
-                              (define-key ido-completion-map (kbd "C-x") 'exit-minibuffer))
-                            ido-setup-hook)))
-  (ido-completing-read "Cached File: "
-                       (mapcar 'car file-cache-alist)))
-  (lusty-file-explorer))
+;; (progn
+;;   (let ((ido-setup-hook (cons (lambda ()
+;;                               (define-key ido-completion-map (kbd "C-x") 'exit-minibuffer))
+;;                             ido-setup-hook)))
+;;   (ido-completing-read "Cached File: "
+;;                        (mapcar 'car file-cache-alist)))
+;;   (lusty-file-explorer))
 
 
 
