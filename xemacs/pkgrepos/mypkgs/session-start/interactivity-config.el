f9c85ac9c894657fdae13d9dec7bfbc9af4ba7ed
Sharad Pratap <sh4r4d@gmail.com>
2013-08-05 20:23:10 +0530

    corrections

--- xemacs/pkgrepos/mypkgs/session-start/interactivity-config.el
+++ xemacs/pkgrepos/mypkgs/session-start/interactivity-config.el
@@ -77,7 +77,34 @@
 
   (deh-require-maybe idomenu
     ;; http://emacswiki.org/emacs/ImenuMode
-    ))
+    )
+
+
+
+
+  (deh-section "bookmark"
+    ;; http://www.emacswiki.org/emacs/InteractivelyDoThings#toc10
+    (require 'bookmark)
+    ;; (setq enable-recursive-minibuffers nil)
+    (define-key ido-file-dir-completion-map [(meta control ?b)] 'ido-goto-bookmark)
+    (defun ido-goto-bookmark (bookmark)
+      (interactive
+       (let ((enable-recursive-minibuffers t))
+           (list (bookmark-completing-read "Jump to bookmark"
+                   bookmark-current-bookmark))))
+      (unless bookmark
+        (error "No bookmark specified"))
+      (let ((filename (bookmark-get-filename bookmark)))
+        (if (file-directory-p filename)
+            (progn
+              (ido-set-current-directory filename)
+              (setq ido-text ""))
+            (progn
+              (ido-set-current-directory (file-name-directory filename))))
+        (setq ido-exit        'refresh
+              ido-text-init   ido-text
+              ido-rotate-temp t)
+        (exit-minibuffer)))))
 
 ;; ido-work-file-list
 ;; ido-work-directory-list
