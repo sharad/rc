e981840c67f51da7a87e5b1a91d6f849b9ff4ce1
Sharad Pratap <sh4r4d@gmail.com>
2014-02-22 15:07:40 +0600

    correct

--- conkerorrc/prefs/prefs.js
+++ conkerorrc/prefs/prefs.js
@@ -6,22 +6,18 @@
  * the changes will be overwritten when the application exits.
  *
  * To make a manual change to preferences, you can visit the URL about:config
- * For more information, see http://www.mozilla.org/unix/customizing.html#prefs
  */
 
-if (false )  {
-user_pref("app.update.lastUpdateTime.addon-background-update-timer", 1290846548);
-user_pref("app.update.lastUpdateTime.background-update-timer", 1290846548);
-user_pref("app.update.lastUpdateTime.blocklist-background-update-timer", 1290846548);
-user_pref("browser.display.background_color", "#000000");
+user_pref("app.update.lastUpdateTime.addon-background-update-timer", 1272248887);
+user_pref("app.update.lastUpdateTime.background-update-timer", 1272248886);
+user_pref("app.update.lastUpdateTime.blocklist-background-update-timer", 1272248886);
 user_pref("extensions.blocklist.pingCountVersion", -1);
-user_pref("extensions.databaseSchema", 3);
-user_pref("extensions.enabledItems", "{6f9d85e0-794d-11dd-ad8b-0800200c9a66}:0.4,mozrepl@hyperstruct.net:2009122217");
-user_pref("extensions.lastAppVersion", "0.9.3 (Debian-0.9.3+git110307-2ubuntu1)");
-user_pref("extensions.mozrepl.autoStart", true);
-user_pref("extensions.newAddons", "{6f9d85e0-794d-11dd-ad8b-0800200c9a66},mozrepl@hyperstruct.net");
+user_pref("extensions.bootstrappedAddons", "{}");
+user_pref("extensions.enabledItems", "");
+user_pref("extensions.installCache", "[]");
+user_pref("extensions.lastAppVersion", "1.0pre (Debian-1.0~~pre+git131029-1)");
+user_pref("extensions.lastPlatformVersion", "27.0");
 user_pref("extensions.update.notifyUser", false);
-user_pref("idle.lastDailyNotification", 1289929857);
 user_pref("network.cookie.prefsMigrated", true);
 user_pref("spellchecker.dictionary", "en-US");
-}
+user_pref("toolkit.telemetry.previousBuildID", "root@lispm.genera.net_1383159010");
