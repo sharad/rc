b9507220e2c88a067fef21ff5a659dd129754e9e
Sharad Pratap <sh4r4d@gmail.com>
2014-02-23 22:16:36 +0530

    correction

--- binx/net/ip/resolveip.pl
+++ binx/net/ip/resolveip.pl
@@ -12,9 +12,15 @@ my $name;
 
 
 if ($addr =~ /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/) {
-    $name = gethostbyaddr(inet_aton($ARGV[0] ),AF_INET)
+    $name = gethostbyaddr(inet_aton( $addr ), AF_INET)
 } else {
-    my $ip = gethostbyname($ARGV[0]);
+    my $ip  = gethostbyname($addr);
+    my $rhost = gethostbyaddr( $ip, AF_INET);
+    # print $rhost . "\n";
+    my $rip = gethostbyname( $rhost ) ;
+    if ($ip ne $rip) {
+        print STDERR "dns reply is not correct " . $ip . " " . $rip . "\n";
+    }
     $name = inet_ntoa($ip);
 }
 
