507e5dfe8e8721397a2390a69253a362f65768d3
Sharad Pratap <sh4r4d@gmail.com>
2014-08-31 19:27:21 +0530

    correction

--- xemacs/pkgrepos/mypkgs/session-start/javascript-config.el
+++ xemacs/pkgrepos/mypkgs/session-start/javascript-config.el
@@ -294,5 +294,24 @@
       )
 (add-to-list 'interpreter-mode-alist '("node" . js-mode))
 
+(deh-require-maybe (and sws-mode jade-mode)
+  ;; https://github.com/brianc/jade-mode
+
+  (deh-section "flymake"
+    (defun flymake-jade-init ()
+      (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                         'flymake-create-temp-intemp))
+             (local-file (file-relative-name
+                          temp-file
+                          (file-name-directory buffer-file-name)))
+             (arglist (list local-file)))
+        (list "jade" arglist)))
+    (setq flymake-err-line-patterns
+          (cons '("\\(.*\\): \\(.+\\):\\([[:digit:]]+\\)$"
+                  2 3 nil 1)
+                flymake-err-line-patterns))
+    (add-to-list 'flymake-allowed-file-name-masks
+                 '("\\.jade\\'" flymake-jade-init))))
+
 (provide 'javascript-config)
 ;;; javascript-config.el ends here
