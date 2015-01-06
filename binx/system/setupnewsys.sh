f7f5765d54f25d6fda9f1933740c502f0380188e
Sharad Pratap <sh4r4d@gmail.com>
2015-01-06 22:49:41 +0530

    corrections

--- binx/system/setupnewsys.sh
+++ binx/system/setupnewsys.sh
@@ -5,8 +5,22 @@
 
 # do apt-clone
 
+
+# check for /atlantic and pacific dirs
+foreach d (/all/reiser /all/res/share) {
+    if [ -d $d ] ; then
+        print $d is presnt
+    else
+        print $d is not presnt, exiting
+        exit -1
+    fi
+}
+
+print Installing sbcl, stow
+sudo apt-get install sbcl stow
+
 if [ ! -d ~/.pi/org ] ; then
-    mkdir -p ~/.pi/org
+    mkdir -p ~/.pi/
     git clone ssh://git@bitbucket.org/sh4r4d/orgp.git ~/.pi/org
 else
     print ~/.pi/org already present
@@ -20,24 +34,51 @@ else
     print ~/.setup-trunk already present
 fi
 
-if [ ! -d ~/.osetup-trunk ] ; then
-    git clone ssh://git@bitbucket.org/sh4r4d/osetup.git ~/osetup-trunk
-    cd ~/; ln -s .osetup-trunk .osetup
-    cp -d ~/.osetup/_home/.* ~/
+
+
+foreach d (  osetup system sysinfo ) {
+if [ ! -d  ] ; then
+    git clone git@github.com:sharad/$d.git ~/.$d-trunk
+    cd ~/;
+    ln -s .$d-trunk .$d
+    cp -d ~/.setup/_home/.* ~/
+    cd -
 else
-    print ~/.osetup-trunk already present
+    print ~/.$d-trunk already present
 fi
+}
 
-print Installing sbcl, stow
-sudo apt-get install sbcl stow
 
-foreach d (/usr/local/stow /usr/local/builds) {
+sudo mkdir -p /usr/local/stow
+foreach d ( /usr/local/builds ) {
     mkdir -p $d
     sudo chown ${USER}:${GID} $d
 
 }
 
+# copy ~/.config/common-lisp
+#depend .setup
+mkdir -p ~/.config/
+cp -r ~/.setup/config/common-lisp ~/.config/
+
+# setup ecryptfs-setup-private
+if [ -d ~/.Private ] ; then
+    print ~/.Private already present
+else
+    ecryptfs-setup-private
+    echo $HOME/.Private > ~/.ecryptfs/Private.mnt
+fi
+
+# setup .ssh/login-keys.d
+#depend .osetup .setup .Private
+mkdir ~/.ssh
+ln -s .setup/ssh/login-keys.d ~/.ssh/login-keys.d
+ls -l ~/.ssh/login-keys.d
+
+
+
 git clone gitolite@lispm:s/lisp/stumpwm.git /usr/local/builds/stumpwm
+git -C /usr/local/builds/stumpwm checkout pa-point-timeout
 
 cd /usr/local/builds/stumpwm/
 sudo rm -f   /usr/local/stow/stumpwm-lispm/bin/stumpwm
@@ -52,14 +93,13 @@ make clean
 ls /usr/local/stow/stumpwm-lispm/bin/stumpwm
 cd -
 
-# check for /atlantic and pacific dirs
-# copy ~/.config/common-lisp
 # copy .osetup
-# setup .ssh/login-keys.d
 # setup quicklisp
 # run sbcl
-# setup ecryptfs-setup-private
 # setup ls .osetup/dirs.d especially reiser-disk and scratch.d/
 # setup mail
 # setup ~/.maildir folder
 # postfix and dovecot
+# compile gnus
+# setup .authinfo
+# setup ecryptfs password
