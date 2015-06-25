721002d15e512833c04021a83f5a3c0ac91f01ba
Sharad Pratap <sh4r4d@gmail.com>
2015-06-25 17:15:55 +0530

    corrections

--- binx/net/ip/fixdnsbind.sh
+++ binx/net/ip/fixdnsbind.sh
@@ -1,5 +1,14 @@
-#/bin/zsh
+#!/bin/zsh
 
 
-resolvednses=( $(grep nameserver /etc/resolv.conf | tr -s  ' \t'  ' ' | cut -d' ' -f2 ) )
-forwarddnses=( $(sed -ne 's/^[ \t]*\([0-9.]\+\);[ \t]*/\1/p' /etc/bind/named.conf.forwarders) )
+resolvednses=( ${(f)"$(grep nameserver /etc/resolv.conf | grep -v 127.0 | tr -s  ' \t'  ' ' | cut -d' ' -f2 )"} )
+forwarddnses=( ${(f)"$(sed -ne 's/^[ \t]*\([0-9.]\+\);[ \t]*/\1/p' /etc/bind/named.conf.forwarders)"} )
+
+dnses=( ${resolvednses} ${forwarddnses} )
+
+print 'forwarders {'
+foreach dns ( ${dnses} ) {
+    print "	"${dns}";"
+}
+print '};'
+print
