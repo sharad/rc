85a01a4506e27764936bdb99451057d089f07981
Sharad Pratap <sh4r4d@gmail.com>
2017-09-07 21:57:56 +0530

    correction

--- xemacs/elpa/pkgs/org-clock-utils-lotus/org-clock-utils-lotus.el
+++ xemacs/elpa/pkgs/org-clock-utils-lotus/org-clock-utils-lotus.el
@@ -352,6 +352,96 @@ EXTRA is additional text that will be inserted into the notes buffer."
         (concat "# Task: " (org-get-heading t) "\n\n")))
       (if fail-quietly (throw 'exit t) (user-error "No active clock"))))
 
+
+(when nil
+
+  ;; from https://www.emacswiki.org/emacs/download/goto-last-change.el
+  ;; from https://www.emacswiki.org/emacs/GotoLastChange
+  ;; from https://www.emacswiki.org/emacs/TrackChanges
+
+  ;; detect changes over the time and do pre-defined actions
+
+
+;;; Code:
+  (provide 'goto-last-change)
+
+  (or (fboundp 'last)			; Emacs 20
+      (require 'cl))			; Emacs 19
+
+  (defvar goto-last-change-undo nil
+    "The `buffer-undo-list' entry of the previous \\[goto-last-change] command.")
+  (make-variable-buffer-local 'goto-last-change-undo)
+
+;;;###autoload
+  (defun goto-last-change (&optional mark-point minimal-line-distance)
+    "Set point to the position of the last change.
+Consecutive calls set point to the position of the previous change.
+With a prefix arg (optional arg MARK-POINT non-nil), set mark so \
+\\[exchange-point-and-mark]
+will return point to the current position."
+    (interactive "P")
+    ;; (unless (buffer-modified-p)
+    ;;   (error "Buffer not modified"))
+    (when (eq buffer-undo-list t)
+      (error "No undo information in this buffer"))
+    (when mark-point
+      (push-mark))
+    (unless minimal-line-distance
+      (setq minimal-line-distance 10))
+    (let ((position nil)
+          (undo-list (if (and (eq this-command last-command)
+                              goto-last-change-undo)
+                         (cdr (memq goto-last-change-undo buffer-undo-list))
+                         buffer-undo-list))
+          undo)
+      (while (and undo-list
+                  (or (not position)
+                      (eql position (point))
+                      (and minimal-line-distance
+                           ;; The first invocation always goes to the last change, subsequent ones skip
+                           ;; changes closer to (point) then minimal-line-distance.
+                           (memq last-command '(goto-last-change
+                                                goto-last-change-with-auto-marks))
+                           (< (count-lines (min position (point-max)) (point))
+                              minimal-line-distance))))
+        (setq undo (car undo-list))
+        (cond ((and (consp undo) (integerp (car undo)) (integerp (cdr undo)))
+               ;; (BEG . END)
+               (setq position (cdr undo)))
+              ((and (consp undo) (stringp (car undo))) ; (TEXT . POSITION)
+               (setq position (abs (cdr undo))))
+              ((and (consp undo) (eq (car undo) t))) ; (t HIGH . LOW)
+              ((and (consp undo) (null (car undo)))
+               ;; (nil PROPERTY VALUE BEG . END)
+               (setq position (cdr (last undo))))
+              ((and (consp undo) (markerp (car undo)))) ; (MARKER . DISTANCE)
+              ((integerp undo))		; POSITION
+              ((null undo))		; nil
+              (t (error "Invalid undo entry: %s" undo)))
+        (setq undo-list (cdr undo-list)))
+      (cond (position
+             (setq goto-last-change-undo undo)
+             (goto-char (min position (point-max))))
+            ((and (eq this-command last-command)
+                  goto-last-change-undo)
+             (setq goto-last-change-undo nil)
+             (error "No further undo information"))
+            (t
+             (setq goto-last-change-undo nil)
+             (error "Buffer not modified")))))
+
+  (defun goto-last-change-with-auto-marks (&optional minimal-line-distance)
+    "Calls goto-last-change and sets the mark at only the first invocations
+in a sequence of invocations."
+    (interactive "P")
+    (goto-last-change (not (or (eq last-command 'goto-last-change-with-auto-marks)
+                               (eq last-command t)))
+                      minimal-line-distance))
+
+  ;; (global-set-key "\C-x\C-\\" 'goto-last-change)
+
+  )
+
 ;;;###autoload
 (defun lotus-org-clock-in/out-insinuate-hooks ()
   (add-hook 'org-clock-in-hook
