320352653fde656f65e0a6a53ffb0cf8702be6b3
Sharad Pratap <sh4r4d@gmail.com>
2017-10-26 23:22:44 +0530

    corrections

--- conkerorrc/office.js
+++ conkerorrc/office.js
@@ -28,3 +28,12 @@ define_webjump("p4cl",
 
 define_webjump("p4describe",
                "http://rain.merunetworks.com:1667/@md=d&cd=//&c=JUj&cl=&ra=s&rc=s&rt=s@/p4%20describe%20%s?ac=160");
+
+define_webjump("mantis-fixforecast",
+               "https://mantis.fortinet.com/view_all_set.php?" +
+               "type=1&view_type=0&sort=last_updated&dir=DESC&page_number=1&reporter_id=any&" +
+               "group_id=any&handler_id=4709&group_type=reporter&qa_assignee_id=any&" +
+               "show_severity=any&show_status=50&css_priority=any&review_required=any&" +
+               "add_to_rn=&search_type=1&filter=Apply&search=&search_fixforecast=%s&" +
+               "eco_checked_in=&escalated_by=&reportsource=any&affectedcustomer=&" +
+               "branch_fix_request=&dev_status=any&qa_test_id=&resolution=any");
