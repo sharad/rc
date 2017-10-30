f79af93d6f71d347bdef17da09e06e88ef5b7e76
Sharad Pratap <sh4r4d@gmail.com>
2017-10-30 23:32:26 +0530

    corrections

--- binx/setup/setup-user.sh
+++ binx/setup/setup-user.sh
@@ -12,7 +12,7 @@ APT_REPO_UTILS="ppa:yartsa/lvmeject"
 DEB_PKG_NECESSARY="git ecryptfs-utils openssl stow sbcl cl-clx-sbcl at gksu openssh-server rcs"
 DEB_PKG_APPEARANCE="lxappearance gnome-tweak-tool gnome-themes-standard libgtk-3-dev"
 DEB_PKG_VIRTURALMACHINE="xrdp rdesktop vncviewer remmina remmina-plugin-rdp"
-DEB_PKGS1="vim emacs emacs-goodies-el org-mode develock-el"
+DEB_PKGS1="vim emacs emacs-goodies-el org-mode develock-el dash-el s-el"
 DEB_PKGS2="rxvt-unicode-256color elscreen planner-el p7zip-full pdftk golang gocode"
 DEB_EXTRA_PKG1=" libpam-tmpdir xdg-utils xdg-user-dirs menu-xdg extra-xdg-menus obsession keyringer menu tree wipe xclip "
 DEB_EXTRA_PKG_COMMUNICATION="pidgin pidgin-skypeweb purple-skypeweb telegram-purple tor"
@@ -22,7 +22,7 @@ DEB_EXTRA_PKG_LISP="cl-swank slime"
 DEB_EXTRA_PKG2="homesick yadm macchanger xautolock suckless-tools xtrlock xbacklight xautomation"
 DEB_EXTRA_PKG3="makepasswd libstring-mkpasswd-perl inotify-tools conky-all macchanger lm-sensors tidy xmlstarlet network-manager-openvpn-gnome "
 DEB_EXTRA_SEC_PKG1="systemd-ui " # policykit-1 policykit-1-gnome
-DEB_DEV_PKG1="python-pip silversearcher-ag ack-grep"
+DEB_DEV_PKG1="python-pip silversearcher-ag silversearcher-ag-el ack-grep"
 DEB_EXTRA_PKG3_UTILS="system-config-lvm lvmeject"
 
 
--- xemacs/elpa/pkgs/org-clock-utils-lotus/org-clock-utils-lotus-pkg.el
+++ xemacs/elpa/pkgs/org-clock-utils-lotus/org-clock-utils-lotus-pkg.el
@@ -1,2 +1,2 @@
-(define-package "org-clock-utils-lotus" "20170919.1744" "org-clock-utils-lotus."
+(define-package "org-clock-utils-lotus" "20171030.36" "org-clock-utils-lotus."
                 '((timer-utils-lotus "20170717.110")))
