9bcab0a9f10960dd6c679d849ec715abf69a9fb2
Sharad Pratap <sh4r4d@gmail.com>
2017-12-04 03:33:36 +0530

    correction

--- xemacs/elpa/pkgs/org-clock-utils-lotus/org-clock-utils-lotus-pkg.el
+++ xemacs/elpa/pkgs/org-clock-utils-lotus/org-clock-utils-lotus-pkg.el
@@ -1,2 +1,3 @@
-(define-package "org-clock-utils-lotus" "20171113.1354" "org-clock-utils-lotus."
-                '((timer-utils-lotus "20170717.110")))
+(define-package "org-clock-utils-lotus" "20171204.327" "org-clock-utils-lotus."
+                '((timer-utils-lotus "20170717.110")
+                  (org-misc-utils-lotus "1")))
