0acebcebd62bcd8d0ad8b55403dcf284240067d1
Sharad Pratap <sh4r4d@gmail.com>
2018-03-08 13:27:09 +0530

    correction

--- xemacs/init.el
+++ xemacs/init.el
@@ -3,6 +3,13 @@
 
 
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (defvar *emacs-in-init* t "Emacs is in init.")
 (defvar user-emacs-directory "~/.emacs.d")
 (setq user-emacs-directory "~/.emacs.d")
