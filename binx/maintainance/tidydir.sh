1d2899c52bdadd504f4179e41fc19ca4ea9c0de9
Sharad Pratap <sh4r4d@gmail.com>
2018-03-09 02:11:54 +0530

    correction

--- binx/maintainance/tidydir.sh
+++ binx/maintainance/tidydir.sh
@@ -6,20 +6,32 @@
 # [[file:~/.setup/binx/maintainance/tidydir.org::*script][script:1]]
 function main()
 {
-  mkdir -p ~/.tidydir
-  dir=$1
-  dirFile=~/.tidydir/$(echo $(readlink -m $dir) | tr -s / | tr / _)
+    if [ ! -d ~/Sink ]
+    then
+        print directory '~/Sink' do not exists >&1
+        exit
+    fi
+    mkdir -p ~/.tidydir
+    dir=$1
+    dirFile=~/.tidydir/$(echo $(readlink -m $dir) | tr -s / | tr / _)
 
-  if [ -e $dirFile ]
-  then
-      command ls -1 > $dirFile
-  else
-      dirFile_tmp=${dirFile}_tmp
-      dirFile_extra=${dirFile}_extra
-      comm -31 $dirFile $dirFile_tmp > $dirFile_extra
-      xargs -d '\n' -a $dirFile_extra echo file extra is
-  fi
+    if [ -e $dirFile ]
+    then
+        dirFile_tmp=${dirFile}_tmp
+        dirFile_extra=${dirFile}_extra
+        command ls -1 > $dirFile_tmp
+        comm -31 $dirFile $dirFile_tmp > $dirFile_extra
+        if [ "$(cat $dirFile_extra |wc -l)" -gt 0 ]
+        then
+            xargs -d '\n' -a $dirFile_extra sh -c 'echo moving "$0" to ~/Sink'
+            xargs -d '\n' -a $dirFile_extra sh -c 'mv "$0" ~/Sink'
+        fi
+        rm -f $dirFile_tmp
+        rm -f $dirFile_extra
+    else
+        command ls -1 > $dirFile
+    fi
 }
 
-main
+main $@
 # script:1 ends here
