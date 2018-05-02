bc60115574f41189c7b7403f50a8acfb3acdaa0a
Sharad Pratap <sh4r4d@gmail.com>
2018-05-02 17:30:49 +0530

    correction

---
+++ binx/auth/pksu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sentinel="##@@##"
+if [[ $(id -u) != "0" ]]; then
+    absScriptPath="$( cd "$(dirname "$0")" ; pwd -P )/$(basename "$0")"
+    pkexec "$absScriptPath" "$sentinel" "$DISPLAY" "$XAUTHORITY" "$DBUS_SESSION_BUS_ADDRESS" "$@"
+    exit 0
+fi
+# root here
+if [[ "$1" == "$sentinel" ]]; then
+    shift
+    export DISPLAY="$1"
+    shift
+    export XAUTHORITY="$1"
+    shift
+    export DBUS_SESSION_BUS_ADDRESS="$1"
+    shift
+fi
+
+$@
