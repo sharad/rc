79b3c68f66947663f084b173341ecd61c01bd508
Sharad Pratap <sh4r4d@gmail.com>
2018-05-30 21:42:22 +0530

    Scripted auto-commit on change (2018-05-30 21:42:22) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-func.el
+++ xemacs/elpa/pkgs/occ/occ-func.el
@@ -29,7 +29,8 @@
         (if heading-prop (org-context-clock-task-set-property task :task-clock-heading-prop heading-prop))
         (if clock-sum (org-context-clock-task-set-property task :task-clock-clock-sum clock-sum))
 
-        (make-occ-task )
+        (make-occ-task :heading heading
+                       :file file)
 
         (let ((inherited-props (org-context-clock-keys-with-operation :getter nil)))
           (dolist (prop inherited-props)
