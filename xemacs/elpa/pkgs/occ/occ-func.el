6ff416fab877387a6b3bba05d71ef6c607faae33
Sharad Pratap <sh4r4d@gmail.com>
2018-05-31 14:44:31 +0530

    Scripted auto-commit on change (2018-05-31 14:44:31) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-func.el
+++ xemacs/elpa/pkgs/occ/occ-func.el
@@ -35,7 +35,8 @@
             (let* ((propstr (if (keywordp prop) (substring (symbol-name prop) 1) (symbol-name prop)))
                    (val (org-entry-get nil propstr t)))
               (unless (org-context-clock-task-get-property task prop)
-                (aref task :plist prop val))))))
+                (push  prop val
+                       (aref task :plist)))))))
       task)))
 
 ;; (defun org-task-collect-task-clock-info ()
