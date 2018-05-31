fed2c25ba6be189217c6e159acc5ddd783b2fa3d
Sharad Pratap <sh4r4d@gmail.com>
2018-05-31 20:53:57 +0530

    Scripted auto-commit on change (2018-05-31 20:53:57) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-object-methods.el
+++ xemacs/elpa/pkgs/occ/occ-object-methods.el
@@ -43,7 +43,7 @@
 
 (cl-defmethod occ-get-property ((task occ-task) prop)
   (if (memq prop (class-slots (aref task 0)))
-      (cl-struct-slot-value 'xpoint prop point)
+      (cl-struct-slot-value  prop task)
     (plist-get (aref task :plist) prop)))
 
 (cl-defmethod occ-set-property ((task occ-task) prop val)
