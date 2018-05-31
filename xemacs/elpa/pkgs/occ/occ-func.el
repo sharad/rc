13c7e4482af7caa7277ad63685c5f5438952bd28
Sharad Pratap <sh4r4d@gmail.com>
2018-05-31 15:05:52 +0530

    Scripted auto-commit on change (2018-05-31 15:05:52) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-func.el
+++ xemacs/elpa/pkgs/occ/occ-func.el
@@ -62,13 +62,14 @@
           (task (cadr (org-element-at-point)))
           (task-content-start ))
       (when heading
-        ;; (if root   (push (cons "Root" root) task))
-        (if marker    (org-context-clock-task-set-property task :task-clock-marker marker))
-        (if file      (org-context-clock-task-set-property task :task-clock-file file))
-        (if point     (org-context-clock-task-set-property task :task-clock-point point))
-        (if heading   (org-context-clock-task-set-property task :task-clock-heading heading))
-        (if heading-prop   (org-context-clock-task-set-property task :task-clock-heading-prop heading-prop))
-        (if clock-sum (org-context-clock-task-set-property task :task-clock-clock-sum clock-sum))
+        (setf task
+              (funcall builder
+                       :heading heading
+                       :file file
+                       :point point
+                       :heading-prop heading-prop
+                       :clock-sum clock-sum
+                       :plist task-plist))
         (let ((inherited-props (org-context-clock-keys-with-operation :getter nil)))
           (dolist (prop inherited-props)
             (let* ((propstr (if (keywordp prop) (substring (symbol-name prop) 1) (symbol-name prop)))
