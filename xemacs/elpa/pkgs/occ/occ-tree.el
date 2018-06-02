edaca5021246ce5124355566359a78c3cc1f5530
Sharad Pratap <sh4r4d@gmail.com>
2018-06-02 20:37:20 +0530

    Scripted auto-commit on change (2018-06-02 20:37:20) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-tree.el
+++ xemacs/elpa/pkgs/occ/occ-tree.el
@@ -121,6 +121,36 @@
             (push (funcall fun) collection))))
     collection))
 
+(defun occ-task-tree-build (collector &optional file)
+  "Build recursive org tasks from org FILE (or current buffer) using COLLECTOR function e.g. occ-task-collect-task"
+  (with-current-buffer (if file
+                           (find-file-noselect file)
+                           (current-buffer))
+    (if file (goto-char (point-min)))
+    (let* ((entry (funcall collector))
+           (sub-tree
+            (append
+             (occ-task-tree-map-subheading 'occ-task-tree-collect-task)
+             (let* ((file (if file file (buffer-file-name)))
+                    (subtree-file
+                     (occ-get-property entry :SUBTREEFILE))
+                    (subtree-file
+                     (if (and subtree-file
+                              (file-relative-name subtree-file))
+                         (expand-file-name subtree-file
+                                           (if file
+                                               (file-name-directory file)
+                                               default-directory))
+                         subtree-file)))
+               (if (and
+                    subtree-file
+                    (file-readable-p subtree-file))
+                   (list
+                    (occ-task-tree-collect-task subtree-file)))))))
+      (if sub-tree
+          (occ-set-property entry 'subtree sub-tree)
+          entry))))
+
 ;;;###autoload
 (defun occ-task-recursive-update-tasks (&optional force) ;; API (occ-api-set :predicate :update  'org-entry-list-update-tasks)
   "Update recursive org tasks tree"
