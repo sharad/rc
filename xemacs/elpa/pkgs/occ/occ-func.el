68d3eda8594555af939ffe8f57c34d9723d818bd
Sharad Pratap <sh4r4d@gmail.com>
2018-06-03 18:32:19 +0530

    Scripted auto-commit on change (2018-06-03 18:32:19) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-func.el
+++ xemacs/elpa/pkgs/occ/occ-func.el
@@ -103,48 +103,6 @@
                 (occ-set-property task prop val))))))
       task)))
 
-;; (defun org-task-collect-task-clock-info ()
-(defun occ-make-task-from-clock (builder)
-  ;; NOT used anywhere
-  ;; (org-element-at-point)
-  (let ((heading-with-string-prop
-         (unless (org-before-first-heading-p)
-           (org-get-heading 'notags))))
-    (let ((heading (if heading-with-string-prop
-                       (substring-no-properties heading-with-string-prop)))
-          (heading-prop (if heading-with-string-prop
-                            heading-with-string-prop))
-          (marker  (move-marker
-                    (make-marker)
-                    (point)
-                    (org-base-buffer (current-buffer))))
-          (file    (buffer-file-name))
-          (point   (point))
-          (clock-sum (if (org-before-first-heading-p)
-                         0
-                         (org-clock-sum-current-item)))
-          (task-plist (cadr (org-element-at-point))))
-      ;; (task-content-start )
-      (when heading
-        (setf task
-              (funcall builder
-                       :name    heading
-                       :heading heading
-                       :file file
-                       :point point
-                       :heading-prop heading-prop
-                       :clock-sum clock-sum
-                       :plist task-plist))
-        (let ((inherited-props (org-context-clock-keys-with-operation :getter nil)))
-          (dolist (prop inherited-props)
-            (let* ((propstr (if (keywordp prop) (substring (symbol-name prop) 1) (symbol-name prop)))
-                   (val (org-entry-get nil propstr t)))
-              (unless (occ-get-property task prop)
-                (occ-set-property task prop val)))))
-        (if heading-with-string-prop
-            (occ-set-property task 'task-clock-content (occ-heading-content-only))))
-      task)))
-
 (defun occ-make-context (&optional buff)
   (let* ((buff (if buff
                    (if (bufferp buff)
