0905d9d258b6191034c15c5ca68a96c72e6dd80a
Sharad Pratap <sh4r4d@gmail.com>
2018-06-03 17:11:37 +0530

    Scripted auto-commit on change (2018-06-03 17:11:37) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-object-methods.el
+++ xemacs/elpa/pkgs/occ/occ-object-methods.el
@@ -78,11 +78,11 @@
   "isassoc")
 
 (cl-defmethod isassoc ((task occ-task) (context occ-context))
-  (reduce #'+
+  ((reduce #'+
           (mapcar
            #'(lambda (slot)
                (isassoc (cons slot task) context))
-           (occ-class-slot (cl-classname task)))))
+           (occ-class-slot (cl-classname task))))))
 
 (cl-defmethod isassoc ((task-pair (head 'root)) (context occ-context))
   "Predicate funtion to check if context matches to task's file attribute."
