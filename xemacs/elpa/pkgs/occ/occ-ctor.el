ff25d3015f3d938216ef97113a85a17ea968df5e
Sharad Pratap <sh4r4d@gmail.com>
2018-06-03 20:00:05 +0530

    Scripted auto-commit on change (2018-06-03 20:00:05) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-ctor.el
+++ xemacs/elpa/pkgs/occ/occ-ctor.el
@@ -188,7 +188,8 @@
 
 (defun occ-collection-object () occ-global-task-collection)
 
-
+(setf occ-gtree
+      (occ-tree-task-collection-tree occ-global-task-collection))
 
 
 (when nil
@@ -203,7 +204,7 @@
    #'(lambda ()
        (or
         (occ-make-task-at-point #'make-occ-list-task)
-        (make-occ-list-task))) ;; note: only using first file of root-files
+        (make-occ-tree-task :name "empty tree task")make-occ-list-task))) ;; note: only using first file of root-files
    org-context-clock-task-tree-task-root-org-file))
 
 (provide 'occ-ctor)
