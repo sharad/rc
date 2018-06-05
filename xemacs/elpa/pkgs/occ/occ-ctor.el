0a0018aca2eb97d392856b39250a50ce52c19eb1
Sharad Pratap <sh4r4d@gmail.com>
2018-06-05 17:17:14 +0530

    Scripted auto-commit on change (2018-06-05 17:17:14) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-ctor.el
+++ xemacs/elpa/pkgs/occ/occ-ctor.el
@@ -126,7 +126,7 @@
                    :buffer buff)))
     context))
 
-(defmethod occ-make-contextual-task ((task occ-task)
+(cl-defmethod occ-make-contextual-task ((task occ-task)
                                      (context occ-context)
                                      (rank number))
   (make-occ-contextual-task
