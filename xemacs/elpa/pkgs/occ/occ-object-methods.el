46a47a9eff78a6019ebfdc183682474e8e4b51f3
Sharad Pratap <sh4r4d@gmail.com>
2018-06-12 14:25:30 +0530

    Scripted auto-commit on change (2018-06-12 14:25:30) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-object-methods.el
+++ xemacs/elpa/pkgs/occ/occ-object-methods.el
@@ -242,12 +242,10 @@
       prompt
       default-directory default-directory))))
 
-(cadar
- (aref
-  (car (aref (cl--generic 'readprop) 3)) 1))
+
 
 (mapcar
- #'(lambda (fspec) ((aref fspec 1)))
+ #'(lambda (fspec) (cadar (aref fspec 1)))
  (aref (cl--generic 'readprop) 3))
 
 (cl-defmethod writeprop ((task-pair (head subtree)))
