6681adbf638cb42527c6ffb1d0ee17ac3a1a938c
Sharad Pratap <sh4r4d@gmail.com>
2018-06-14 19:19:34 +0530

    Scripted auto-commit on change (2018-06-14 19:19:34) by gitwatch.sh

--- xemacs/elpa/pkgs/task-manager/task-manager.el
+++ xemacs/elpa/pkgs/task-manager/task-manager.el
@@ -857,7 +857,7 @@ which other peoples are also working."
           (when (or
                  (eq major-mode 'c-mode)
                  (eq major-mode 'c++-mode))
-            (setq tab-width (custom-reevaluate-setting)
+            (setq tab-width (custom-reevaluate-setting 'tab-width) )
             (c-set-style "stroustrup" 1))
           (set (make-local-variable 'before-save-hook) before-save-hook)
           (remove-hook 'before-save-hook 'delete-trailing-whitespace t)
