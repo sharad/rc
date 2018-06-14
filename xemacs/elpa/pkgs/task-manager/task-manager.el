be3003ee8e01899f1b4115273c1108dbcb5af5c2
Sharad Pratap <sh4r4d@gmail.com>
2018-06-14 19:17:59 +0530

    Scripted auto-commit on change (2018-06-14 19:17:59) by gitwatch.sh

--- xemacs/elpa/pkgs/task-manager/task-manager.el
+++ xemacs/elpa/pkgs/task-manager/task-manager.el
@@ -861,7 +861,7 @@ which other peoples are also working."
             (c-set-style "stroustrup" 1))
           (set (make-local-variable 'before-save-hook) before-save-hook)
           (remove-hook 'before-save-hook 'delete-trailing-whitespace t)
-          (message "called office mode")))
+          (message "called disable office mode")))
     (error (message "Error: %s" e))))
 
 (when nil
