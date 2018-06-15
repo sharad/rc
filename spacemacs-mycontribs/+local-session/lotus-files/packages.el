a8556a484d7fd7f55cbf998fb90abe2e1e48fed1
Sharad Pratap <sh4r4d@gmail.com>
2018-06-15 12:20:32 +0530

    Scripted auto-commit on change (2018-06-15 12:20:32) by gitwatch.sh

--- spacemacs-mycontribs/+local-session/lotus-files/packages.el
+++ spacemacs-mycontribs/+local-session/lotus-files/packages.el
@@ -604,12 +604,13 @@ timestamp has not changed (e.g. with ftp or on Windows)."
                         (exit-minibuffer))))
 
                 ;; ido-file-completion-map is only defined when ido-mode is called.
-                (add-hook 'ido-setup-hook
-                          (lambda ()
-                            (keymap-set-key-if-unbind
-                             ido-file-completion-map
-                             (kbd "C-.")
-                             'ido-plain-directory))))))))
+                ;; (add-hook 'ido-setup-hook
+                ;;           (lambda ()
+                ;;             (keymap-set-key-if-unbind
+                ;;              ido-file-completion-map
+                ;;              (kbd "C-.")
+                ;;              'ido-plain-directory)))
+                )))))
   (when nil
 
     ;; set bindings
