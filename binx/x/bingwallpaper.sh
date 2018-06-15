021effea7f2ad2b27b8af2de42e4b8b5d63d9c72
Sharad Pratap <sh4r4d@gmail.com>
2018-06-15 15:52:27 +0530

    Scripted auto-commit on change (2018-06-15 15:52:27) by gitwatch.sh

--- binx/x/bingwallpaper.sh
+++ binx/x/bingwallpaper.sh
@@ -90,6 +90,7 @@ function main() {
 function resolveImg() {
     reqImgURL="$1"
     localPath="$2"
+    curl -s -o $imgpath$imgName $reqImgURL
     if file $reqImgURL | grep -q HTML
     then
         echo $(xmlstarlet sel -t -v  "html/body/h2/a/@href" $reqImgURL)
