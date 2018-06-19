49776024f418b2de21d4ed4aecb56a49e73b488b
Sharad Pratap <sh4r4d@gmail.com>
2018-06-20 00:20:15 +0530

    Scripted auto-commit on change (2018-06-20 00:20:15) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-main.el
+++ xemacs/elpa/pkgs/occ/occ-main.el
@@ -59,7 +59,7 @@
               (if (and
                    (not (occ-clock-marker-is-unnamed-clock-p))
                    ;; (> (occ-current-task-associated-to-ctx-p ctx) 0)
-                   (> (occ-associated-p (occ-current-task) ctx)0)
+                   (> (occ-associated-p (occ-current-task) ctx) 0)
                    )
                   (occ-debug :debug "occ-update-current-ctx: Current task already associate to %s" ctx)
                   (progn                ;current clock is not matching
