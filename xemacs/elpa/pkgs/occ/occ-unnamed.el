dcc364d1fc2eb26d2c0303182baa1c1d5e90f716
Sharad Pratap <sh4r4d@gmail.com>
2018-06-19 18:24:38 +0530

    Scripted auto-commit on change (2018-06-19 18:24:38) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-unnamed.el
+++ xemacs/elpa/pkgs/occ/occ-unnamed.el
@@ -26,19 +26,19 @@
 
 (require 'occ-object-methods)
 
-(defvar *occ-unassociate-context-start-time* nil)
+(defvar *occ-unassociate-ctx-start-time* nil)
 (defvar *occ-swapen-unnamed-threashold-interval* (* 60 2)) ;2 mins
 
-(defun occ-unassociate-context-start-time-reset ()
-  (setq *occ-unassociate-context-start-time* nil))
+(defun occ-unassociate-ctx-start-time-reset ()
+  (setq *occ-unassociate-ctx-start-time* nil))
 
 (defun occ-can-create-unnamed-task-p ()
-  (unless *occ-unassociate-context-start-time*
-    (setq *occ-unassociate-context-start-time* (current-time)))
-  (let ((unassociate-context-start-time *occ-unassociate-context-start-time*))
+  (unless *occ-unassociate-ctx-start-time*
+    (setq *occ-unassociate-ctx-start-time* (current-time)))
+  (let ((unassociate-ctx-start-time *occ-unassociate-ctx-start-time*))
     (prog1
         (>
-         (float-time (time-since unassociate-context-start-time))
+         (float-time (time-since unassociate-ctx-start-time))
          *occ-swapen-unnamed-threashold-interval*))))
 
 (defun occ-clock-marker-is-unnamed-clock-p (&optional clock)
@@ -48,7 +48,7 @@
            (lotus-org-unnamed-task-clock-marker))
      (equal
       (marker-buffer org-clock-marker)
-      ;; id:x11 make org-context-clock version
+      ;; id:x11 make org-ctx-clock version
       (marker-buffer (lotus-org-unnamed-task-clock-marker))))))
 
 (defun occ-maybe-create-clockedin-unnamed-heading ()
@@ -58,7 +58,7 @@
           (occ-debug :debug "occ-maybe-create-unnamed-task: Already clockin unnamed task")
           (prog1
               (lotus-org-create-unnamed-task-task-clock-in)
-            (occ-unassociate-context-start-time-reset))))))
+            (occ-unassociate-ctx-start-time-reset))))))
 
 (defun occ-maybe-create-unnamed-heading ()
   (when (occ-can-create-unnamed-task-p)
@@ -77,30 +77,30 @@
            (occ-make-task unnamed-heading-marker (occ-task-builder)))))
     unnamed-task))
 
-(cl-defmethod occ-maybe-create-unnamed-contextual-task ((context occ-context))
+(cl-defmethod occ-maybe-create-unnamed-ctxual-task ((ctx occ-ctx))
   ;; back
   (let* ((unnamed-task
          (occ-maybe-create-unnamed-task))
-        (unnamed-contextual-task
+        (unnamed-ctxual-task
          (when unnamed-task
-           (occ-build-contextual-task unnamed-task context))))
-    unnamed-contextual-task))
+           (occ-build-ctxual-task unnamed-task ctx))))
+    unnamed-ctxual-task))
 
-(defun occ-maybe-create-clockedin-unnamed-contextual-task ((context occ-context))
+(defun occ-maybe-create-clockedin-unnamed-ctxual-task ((ctx occ-ctx))
   ;; back
   (when (occ-can-create-unnamed-task-p)
     (let ((org-log-note-clock-out nil))
       (if (occ-clock-marker-is-unnamed-clock-p)
           (occ-debug :debug "occ-maybe-create-unnamed-task: Already clockin unnamed task")
-        (let* ((unnamed-contextual-task (occ-maybe-create-unnamed-contextual-task context))
-               (unnamed-task            (occ-contextual-task-task unnamed-contextual-task))
+        (let* ((unnamed-ctxual-task (occ-maybe-create-unnamed-ctxual-task ctx))
+               (unnamed-task            (occ-ctxual-task-task unnamed-ctxual-task))
                (unnamed-marker          (occ-task-marker unnamed-task)))
             (prog1
-                (occ-clockin-contextual-task unnamed-contextual-task)
-              ;; id:x11 make org-context-clock version
+                (occ-clockin-ctxual-task unnamed-ctxual-task)
+              ;; id:x11 make org-ctx-clock version
               (lotus-org-unnamed-task-clock-marker unnamed-marker)
               (message "clockin to unnnamed task.")
-              (occ-unassociate-context-start-time-reset)))))))
+              (occ-unassociate-ctx-start-time-reset)))))))
 
 (defun occ-changable-p ()
   "Stay with a clock at least 2 mins."
