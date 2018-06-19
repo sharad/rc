a9c924c54fdef8bc3ebfe68a09d58c0d8e027e5a
Sharad Pratap <sh4r4d@gmail.com>
2018-06-19 18:14:35 +0530

    Scripted auto-commit on change (2018-06-19 18:14:35) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-main.el
+++ xemacs/elpa/pkgs/occ/occ-main.el
@@ -34,7 +34,8 @@
 
 (setq occ-global-task-collection-spec
       (list :tree org-context-clock-task-tree-task-root-org-file))
-(defun occ-set-global-task-collection-spec)
+(defun occ-set-global-task-collection-spec (spec)
+  )
 
 (defun occ-update-current-context (&optional force)
   (interactive "P")
