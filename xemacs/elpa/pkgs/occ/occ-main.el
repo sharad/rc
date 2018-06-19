43ab7bfab7ecb0f4db1622be3bc2a969d607bd0e
Sharad Pratap <sh4r4d@gmail.com>
2018-06-19 21:44:42 +0530

    Scripted auto-commit on change (2018-06-19 21:44:42) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-main.el
+++ xemacs/elpa/pkgs/occ/occ-main.el
@@ -73,7 +73,7 @@
     (occ-debug :debug "occ-update-current-ctx: not enough time passed.")))
 
 
-(defun occ-update-current-ctx (&optional force)
+(cl-defmethod occ-update-current-ctx (&optional force)
   (interactive "P")
   (if (>
        (float-time (time-since *occ-last-buffer-select-time*))
