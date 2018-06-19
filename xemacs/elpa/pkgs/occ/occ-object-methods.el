a6a23272131af63c7a58eb5c813c395ff2684a2a
Sharad Pratap <sh4r4d@gmail.com>
2018-06-19 18:21:25 +0530

    Scripted auto-commit on change (2018-06-19 18:21:25) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-object-methods.el
+++ xemacs/elpa/pkgs/occ/occ-object-methods.el
@@ -490,7 +490,7 @@ pointing to it."
 
 ;; ISSUE? should it return rank or occ-contextual-tasks list
 (cl-defmethod occ-matching-contextual-tasks :around ((collection occ-list-task-collection)
-                                             (context occ-context))
+                                                     (context occ-context))
   (lexical-let ((tasks (occ-collection collection))
                 (context context))
     (remove-if-not
