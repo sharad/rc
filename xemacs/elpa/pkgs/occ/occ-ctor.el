fd62404b4e132e2be1f51fc038cf60b8e10a7174
Sharad Pratap <sh4r4d@gmail.com>
2018-06-19 17:44:33 +0530

    Scripted auto-commit on change (2018-06-19 17:44:33) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-ctor.el
+++ xemacs/elpa/pkgs/occ/occ-ctor.el
@@ -181,7 +181,7 @@
 
 (cl-defmethod occ-collection ((collection occ-tree-task-collection))
   (unless (occ-tree-task-collection-tree occ-global-task-collection)
-    (occ-collect-tasks)))
+    (occ-collect-tasks occ-global-task-collection)))
 
 (cl-defmethod occ-collection ((collection occ-list-task-collection))
   (occ-tree-task-collection-list occ-global-task-collection))
