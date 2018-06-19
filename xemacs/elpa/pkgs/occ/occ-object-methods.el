cd076c45152317d74b53b815fe93fb40cfab36f5
Sharad Pratap <sh4r4d@gmail.com>
2018-06-19 14:01:41 +0530

    Scripted auto-commit on change (2018-06-19 14:01:41) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-object-methods.el
+++ xemacs/elpa/pkgs/occ/occ-object-methods.el
@@ -132,9 +132,9 @@
 
 ;; [[file:~/.repos/git/main/resource/userorg/main/readwrite/public/user/rc/xemacs/elpa/pkgs/org-context-clock/org-context-clock.org::*Collect%20and%20return%20task%20matching%20to%20CONTEXT][Collect and return task matching to CONTEXT:1]]
 ;;;###autoload
-(defun occ-current-task-associated-to-context-p (context)
+(defun occ-current-associated-p (context)
   (let ((task (occ-task-current-task)))
-    (occ-task-associated-to-context-p task context)))
+    (occ-isassoc task context)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
