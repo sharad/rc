19a221ab61641bf1175fa00e90bbeff685791303
Sharad Pratap <sh4r4d@gmail.com>
2018-06-22 19:10:29 +0530

    Scripted auto-commit on change (2018-06-22 19:10:29) by gitwatch.sh

--- xemacs/elpa/pkgs/sessions-unified/sessions-unified.el
+++ xemacs/elpa/pkgs/sessions-unified/sessions-unified.el
@@ -366,8 +366,8 @@ return a new alist whose car is the new pair and cdr is ALIST."
 
                 (while buff-files
 
-                  (unless (eq screen 0)
-                    (elscreen-create))
+                  ;; (unless (eq screen 0)
+                  ;;   (elscreen-create))
 
                   (let* ((buff-file  (car buff-files))
                          (file-path  (if (consp buff-file)
