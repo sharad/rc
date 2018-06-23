f305fdb4e652062c4bb556648decd6ff51a871d5
Sharad Pratap <sh4r4d@gmail.com>
2018-06-24 00:43:00 +0530

    Scripted auto-commit on change (2018-06-24 00:43:00) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-object-methods.el
+++ xemacs/elpa/pkgs/occ/occ-object-methods.el
@@ -75,7 +75,7 @@
      slots)))
 (cl-defmethod cl-method-matched-arg ((method symbol) &optional (ctx symbol))
   (cl-method-first-arg method))
-(cl-defmethod cl-method-matched-arg ((method symbol) (ctx occ-ctx))
+(cl-defmethod cl-method-matched-arg ((method symbol) &optional (ctx occ-ctx))
   (let ((slots (occ-obj-defined-slots ctx)))
   (remove-if-not
    (lambda (arg) (memq arg slots))
