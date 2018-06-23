2e1a6dc76df3f94cbf4a6653becd5ffc492e0d58
Sharad Pratap <sh4r4d@gmail.com>
2018-06-23 19:55:08 +0530

    Scripted auto-commit on change (2018-06-23 19:55:08) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-main.el
+++ xemacs/elpa/pkgs/occ/occ-main.el
@@ -52,7 +52,8 @@
         ;; not able to find associated, or intentionally not selecting a clock
         (occ-debug :debug "trying to create unnamed tsk.")
         (occ-maybe-create-clockedin-unnamed-ctxual-tsk ctx))
-      (occ-debug :debug "occ-update-current-ctx: Now really clock done."))))
+      (occ-debug :debug "occ-update-current-ctx: Now really clock done.")
+      t)))
 
 (cl-defmethod occ-clockin-assoctsk-if-chg ((ctx occ-ctx))
   (if (>
