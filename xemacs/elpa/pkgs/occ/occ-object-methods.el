7f278f228b375f3abfdc0e59579c6f458e2d5252
Sharad Pratap <sh4r4d@gmail.com>
2018-06-24 19:57:40 +0530

    Scripted auto-commit on change (2018-06-24 19:57:39) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-object-methods.el
+++ xemacs/elpa/pkgs/occ/occ-object-methods.el
@@ -488,6 +488,8 @@ pointing to it."
 (cl-defmethod occ-included-files ()
   (occ-collect-included-files (occ-collection-object) nil) )
 
+(occ-tree-tsk-collection-included-files)
+
 ;; ISSUE? should it return rank or occ-ctxual-tsks list
 (cl-defmethod occ-matching-ctxual-tsks ((collection occ-list-tsk-collection)
                                         (ctx occ-ctx))
