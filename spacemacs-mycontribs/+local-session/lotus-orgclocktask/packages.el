58c750f1dabbe2eef6083ac80461924ab47bcbc7
Sharad Pratap <sh4r4d@gmail.com>
2018-06-24 21:13:24 +0530

    Scripted auto-commit on change (2018-06-24 21:13:24) by gitwatch.sh

--- spacemacs-mycontribs/+local-session/lotus-orgclocktask/packages.el
+++ spacemacs-mycontribs/+local-session/lotus-orgclocktask/packages.el
@@ -538,7 +538,7 @@ Each entry is either:
                                   (file-exists-p start-file))
                                  (progn
                                    (if (functionp 'occ-set-global-tsk-collection-spec)
-                                       (occ-setup-task-tree-task-root-org-file start-file)
+                                       (occ-set-global-tsk-collection-spec (list :tree start-file))
                                        (warn "function occ-setup-task-tree-task-root-org-file not available.")))
                                  (message "org party dir %s or file %s not exists."
                                           party-base-dir
