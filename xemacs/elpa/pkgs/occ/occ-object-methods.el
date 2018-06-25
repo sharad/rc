5d0fa9a562ef0c19633571d64e5d0fbd2fa2ed6a
Sharad Pratap <sh4r4d@gmail.com>
2018-06-25 13:52:20 +0530

    Scripted auto-commit on change (2018-06-25 13:52:20) by gitwatch.sh

--- xemacs/elpa/pkgs/occ/occ-object-methods.el
+++ xemacs/elpa/pkgs/occ/occ-object-methods.el
@@ -145,7 +145,7 @@
 ;; Test if TSK is associate to CTX:1 ends here
 
 ;; Collect and return tsk matching to CTX
-;;;###autoload
+;;---------------------------------------------------------------;;;###autoload
 (cl-defmethod occ-current-associated-p ((ctx occ-ctx))
   (let ((tsk (occ-tsk-current-tsk)))
     (when tsk (occ-rank tsk ctx))))
