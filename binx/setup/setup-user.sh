181aea3c80e03a114ffa9043f526668b4aeebc36
Sharad Pratap <sh4r4d@gmail.com>
2018-06-28 12:33:05 +0530

    Scripted auto-commit on change (2018-06-28 12:33:05) by gitwatch.sh

--- binx/setup/setup-user.sh
+++ binx/setup/setup-user.sh
@@ -4,9 +4,9 @@ DEBUG=1
 
 SSH_KEY_DUMP=$1
 TMPDIR=~/setuptmp
-if [ ! -r ~/.ssh/authorized_keys ]
+if [ -r ~/.ssh/authorized_keys ]
 then
-    GIT_SSH_OPTION="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+    GIT_SSH_OPTION="ssh -o UserKnownHostsFile=~/.ssh/authorized_keys -o StrictHostKeyChecking=no"
 else
     GIT_SSH_OPTION="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 fi
