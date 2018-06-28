3548895c617db607b9428a6370316f2594dcda2a
Sharad Pratap <sh4r4d@gmail.com>
2018-06-28 20:40:37 +0530

    Scripted auto-commit on change (2018-06-28 20:40:37) by gitwatch.sh

--- xemacs/snippets/misc.el
+++ xemacs/snippets/misc.el
@@ -20,6 +20,25 @@
 
 
 
+(defun org-copy-block ()
+  (interactive)
+  (org-narrow-to-block)
+  (let ((min
+         (save-excursion
+           (goto-char (point-min))
+           (next-line)
+           (beginning-of-line)
+           (point)))
+        (max
+         (save-excursion
+           (goto-char (point-max))
+           (previous-line)
+           (end-of-line)
+           (point))))
+    (kill-new (buffer-substring min max)))
+  (widen))
+
+
 
 
 
