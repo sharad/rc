e56f3b5cb4f959fdf4eee9aca78d90474005e0b1
Sharad Pratap <sh4r4d@gmail.com>
2018-07-09 15:59:11 +0530

    Scripted auto-commit on change (2018-07-09 15:59:10) by gitwatch.sh

---
+++ xemacs/elpa/pkgs/activity/activity-note.el
@@ -0,0 +1,47 @@
+;;; activity-note.el --- Emacs Activity logger, analyzer and reporter  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  sharad
+
+;; Author: sharad <sh4r4d@gmail.com>
+;; Keywords: data
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This package meant to log, analyze and report all emacs activity of
+;; user which could further utilized to visualize activity of user
+;; during period of time or editing session.
+
+;; Enable Activity for the current buffer by invokingi
+;; `activity-mode'. If you wish to activate it globally, use
+;; `global-activity-mode'.
+
+;; Set variable `activity-api-key' to your API key. Point
+;; `activity-cli-path' to the absolute path of the CLI script
+;; (activity-cli.py).
+
+;; See http://nullprogram.com/blog/2013/04/07/ for help
+;; add example code directly here for quick reference.
+
+;;; Code:
+
+(require '@)
+
+(provide 'activity-note)
+
+
+
+
+;;; activity-note.el ends here
