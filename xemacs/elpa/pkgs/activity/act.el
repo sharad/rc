c65dc2d938073b359f935d18cc5bb3230fcaf750
Sharad Pratap <sh4r4d@gmail.com>
2018-07-16 20:53:37 +0530

    Scripted auto-commit on change (2018-07-16 20:53:37) by gitwatch.sh

--- xemacs/elpa/pkgs/activity/act.el
+++ xemacs/elpa/pkgs/activity/act.el
@@ -83,7 +83,7 @@
           (dolist (dest @:dests)
             (if (and
                  dest
-                 (@! dest :receive fmt args))
+                 (@! dest :keyp fmt args))
                 (@! dest :receive fmt args)
               (message "dest is nil, not sending msg.")))
         (error "No @:dests %d boundp(%s) consp(%s) present."
