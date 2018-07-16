b958e678e166e04de7bcf2681e8e4d9a520bf528
Sharad Pratap <sh4r4d@gmail.com>
2018-07-16 21:18:02 +0530

    Scripted auto-commit on change (2018-07-16 21:18:02) by gitwatch.sh

--- xemacs/elpa/pkgs/activity/act.el
+++ xemacs/elpa/pkgs/activity/act.el
@@ -140,7 +140,7 @@
 
   (@! (@! @note-class :gen-few-msg "message") :send "Test %d" 00)
 
-  (@ (@! @note-class :gen-few-msg "message") :dests)
+  (length (@ (@! @note-class :gen-few-msg "message") :dests))
 
   ;; (@! (@! @dest-class :gen-warning "warning") :receive "Hello")
   ;; (@! (@! @dest-class :gen-msg "msg") :receive "Hello")
