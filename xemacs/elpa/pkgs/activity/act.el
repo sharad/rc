0b6a83184f6c2a0bf1c1c934803bd60b0fccce4d
Sharad Pratap <sh4r4d@gmail.com>
2018-07-16 21:00:36 +0530

    Scripted auto-commit on change (2018-07-16 21:00:36) by gitwatch.sh

--- xemacs/elpa/pkgs/activity/act.el
+++ xemacs/elpa/pkgs/activity/act.el
@@ -72,50 +72,51 @@
 ;; (@! (@! @dest-class :gen-msg ) :receive "Hello")
 
 
-(setf @note-class
-  (@extend @act-base
-           :name "note class"))
+(progn
+ (setf @note-class
+       (@extend @act-base
+                :name "note class"))
 
-(def@ @note-class :send (fmt args)
-      (if (and (memq :dests (@:keys))
-               (consp @:dests))
-          (dolist (dest @:dests)
-            (if dest
-                (if (@! dest :keyp :receive)
-                    (@! dest :receive fmt args)
-                  (message
-                   "dest %s [%s] not has :receive method, not sending msg."
-                   (@ dest :name)
-                   (@! dest :keys)))
-              (message "dest is nil")))
-        (error "No @:dests %d boundp(%s) consp(%s) present."
-               (length @:dests)
-               (boundp '@:dests)
-               (consp @:dests))))
+ (def@ @note-class :send (fmt args)
+       (if (and (memq :dests (@:keys))
+                (consp @:dests))
+           (dolist (dest @:dests)
+             (if dest
+                 (if (@! dest :keyp :receive)
+                     (@! dest :receive fmt args)
+                   (message
+                    "dest %s [%s] not has :receive method, not sending msg."
+                    (@ dest :name)
+                    (@! dest :keys)))
+               (message "dest is nil")))
+         (error "No @:dests %d boundp(%s) consp(%s) present."
+                (length @:dests)
+                (boundp '@:dests)
+                (consp @:dests))))
 
-(defsubclass-gen@ @note-class :gen-format-msg ()
-  (push
-   (@! @dest-class :gen-msg "msg")
-   @:dests)
-  )
+ (defsubclass-gen@ @note-class :gen-format-msg ()
+   (push
+    (@! @dest-class :gen-msg "msg")
+    @:dests)
+   )
 
-(defsubclass-gen@ @note-class :gen-org-log-note ()
-  (push
-   (@! @dest-class :gen-msg "msg")
-   @:dests)
-  )
+ (defsubclass-gen@ @note-class :gen-org-log-note ()
+   (push
+    (@! @dest-class :gen-msg "msg")
+    @:dests)
+   )
 
-(defsubclass-gen@ @note-class :gen-org-dual-log-note ()
-  (push
-   (@! @dest-class :gen-msg "msg")
-   @:dests)
-  )
+ (defsubclass-gen@ @note-class :gen-org-dual-log-note ()
+   (push
+    (@! @dest-class :gen-msg "msg")
+    @:dests)
+   )
 
-(defsubclass-gen@ @note-class :gen-org-intreactive-log-note ()
-  (push
-   (@! @dest-class :gen-msg "msg")
-   @:dests)
-  )
+ (defsubclass-gen@ @note-class :gen-org-intreactive-log-note ()
+   (push
+    (@! @dest-class :gen-msg "msg")
+    @:dests)
+   ))
 
 (@! (@! @note-class :gen-format-msg "message") :send "Test %d" 00)
 
