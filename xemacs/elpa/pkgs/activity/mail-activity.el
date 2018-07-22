8fc6fac13d82b1099ec2661ffc3e83aecb036dd8
Sharad Pratap <sh4r4d@gmail.com>
2018-07-22 21:10:41 +0530

    Scripted auto-commit on change (2018-07-22 21:10:41) by gitwatch.sh

--- xemacs/elpa/pkgs/activity/mail-activity.el
+++ xemacs/elpa/pkgs/activity/mail-activity.el
@@ -41,7 +41,7 @@
 
 (provide 'mail-event)
 
-(defsubclass-gen@ @event-dectector-class :gen-buffer-trans (&optional note)
+(defsubclass-gen@ @event-dectector-class :gen-mail-read-event (&optional note)
   (def@ @@ :make-event ()
     "Make buffer change event."
     (let ((curr (current-buffer)))
