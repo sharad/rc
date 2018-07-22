9d0954d7cc95ed4309c8246acd4790c971adc3ad
Sharad Pratap <sh4r4d@gmail.com>
2018-07-22 20:11:16 +0530

    Scripted auto-commit on change (2018-07-22 20:11:16) by gitwatch.sh

--- xemacs/elpa/pkgs/activity/org-activity-note.el
+++ xemacs/elpa/pkgs/activity/org-activity-note.el
@@ -55,7 +55,10 @@
            (cond
              ((markerp marker) marker)
              ((functionp marker)
-              (let ((m (marker))))))))
+              (let ((m (marker))
+                    (if (markerp m)
+                        m
+                      (error "no marker %s" marker))))))))
      (org-insert-log-note marker (apply #'format fmt args) 'note)))
 
   (@:dispatch marker))
