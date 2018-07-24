0b356b562da3b0b4a476773a36eb5318da9ae7a0
Sharad Pratap <sh4r4d@gmail.com>
2018-07-24 16:44:13 +0530

    Scripted auto-commit on change (2018-07-24 16:44:13) by gitwatch.sh

--- xemacs/elpa/pkgs/activity/activity-base.el
+++ xemacs/elpa/pkgs/activity/activity-base.el
@@ -231,13 +231,13 @@
             )))
 
   (setf @event-dectector-class
-        (defsubobj@ @activity-dectector-class "event detector class" ()
+        (defsubobj@ @activity-dectector-class "event detector class"
           "Event detector class"
           (def@ @@ :note ()
             )))
 
   (setf @transition-dectector-class
-        (defsubobj@ @event-dectector-class "transition detector class" ()
+        (defsubobj@ @event-dectector-class "transition detector class"
           "Transition detector class"
           (def@ @@ :note ()
             ))))
