a1384e0bbfc023ac31de780cd7cc528f5bc75a0b
Sharad Pratap <sh4r4d@gmail.com>
2018-07-26 03:47:42 +0530

    Scripted auto-commit on change (2018-07-26 03:47:42) by gitwatch.sh

--- xemacs/elpa/pkgs/activity/act.el
+++ xemacs/elpa/pkgs/activity/act.el
@@ -400,24 +400,6 @@
 
 
 
-(defun @--replace (symbol head)
-  "Replace @: and @^: symbols with their lookup/funcall expansions."
-  (let ((name (symbol-name symbol)))
-    (cond
-      ((string-match "^@@+$" name)
-       (intern (concat name "@")))
-      ((string-prefix-p "@:" name)
-           (let ((property (intern (substring name 1))))
-             (if head
-                 `(@! @@ ,property)
-               `(@ @@ ,property))))
-      ((string-prefix-p "@^:" name)
-       (let ((property (intern (substring name 2))))
-         (if head
-             `(funcall (@ @@@ ,property :super t) @@)
-           `(@ @@ ,property :super t))))
-      (t (if head (list symbol) symbol)))))
-
 
 (progn
   (setf @test-base
