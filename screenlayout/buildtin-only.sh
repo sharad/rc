11210d44a0ed18d57859fd7909f1ed8ffd01ece7
Sharad Pratap <sh4r4d@gmail.com>
2018-08-07 15:24:28 +0530

    Scripted auto-commit on change (2018-08-07 15:24:27) by gitwatch.sh

--- screenlayout/buildtin-only.sh
+++ screenlayout/buildtin-only.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 xrandr \
-    --output VIRTUAL-1 \
+    --output LVDS-1 \
     --off --output DP-3 \
     --off --output DP-2 \
     --off --output DP-1 \
--- screenlayout/dual-monitor.randr.sh
+++ screenlayout/dual-monitor.randr.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 xrandr \
-    --output VIRTUAL-1 \
+    --output VGA-1 \
     --off --output DP-3 \
     --off --output DP-2 \
     --off --output DP-1 \
@@ -9,4 +9,4 @@ xrandr \
     --off --output HDMI-1 \
     --off --output LVDS-1 \
     --primary --mode 1600x900 --pos 1366x0 --rotate normal \
-    --output VGA1 --mode 1366x768 --pos 0x0 --rotate normal
+    --output VGA-1 --mode 1366x768 --pos 0x0 --rotate normal
