c6627bf32321ee04e366a0c017bb32facfe9de7e
Sharad Pratap <sh4r4d@gmail.com>
2018-08-22 20:23:18 +0530

    Scripted auto-commit on change (2018-08-22 20:23:18) by gitwatch.sh

--- xemacs/elpa/pkgs/activity/activity-base.el
+++ xemacs/elpa/pkgs/activity/activity-base.el
@@ -248,6 +248,11 @@
           (def@ @@ :note ()
             )
           (def@ @@ :dispatch ()
+            (setf
+             @:start-time 0
+             @:stop-time  0
+             @:active-time 0
+             @:inactive-time 0)
             )))
 
   (setf @transition-dectector-class
