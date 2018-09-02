261ba917699696dfa25d09e1f3058c8d54e779dd
Sharad Pratap <sh4r4d@gmail.com>
2018-09-02 17:30:02 +0530

    Scripted auto-commit on change (2018-09-02 17:30:02) by gitwatch.sh

--- xemacs/elpa/pkgs/org-clock-resolve-advanced/org-clock-resolve-advanced.el
+++ xemacs/elpa/pkgs/org-clock-resolve-advanced/org-clock-resolve-advanced.el
@@ -805,8 +805,7 @@ so long."
                     (message "hookfn: with-selected-frame running timer")
                     (run-with-timer 1 0
                                     (lambda nil
-                                      (with-frame-event
-                                          (funcall readfn))))
+                                      (funcall readfn)))
                     (message "hookfn: adding quiet-sel-frame")
                     (add-function :override (symbol-function (quote select-frame-set-input-focus)) (function quiet--select-frame))
                     (message "hookfn: going to run abort-recursive-edit")
