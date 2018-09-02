fd46f5f7dc0554b0b65941f78871f33828958296
Sharad Pratap <sh4r4d@gmail.com>
2018-09-02 17:23:14 +0530

    Scripted auto-commit on change (2018-09-02 17:23:14) by gitwatch.sh

--- xemacs/elpa/pkgs/org-clock-resolve-advanced/org-clock-resolve-advanced.el
+++ xemacs/elpa/pkgs/org-clock-resolve-advanced/org-clock-resolve-advanced.el
@@ -755,7 +755,7 @@ so long."
                                     hookfn))
                    (progn
                      (with-selected-frame last-event-frame
-                       (message "hookfn: removing hook")
+                       (message "hookfn: with-selected-frame running timer")
                        (run-with-timer 1 0
                                        (lambda ()
                                          (with-frame-event
