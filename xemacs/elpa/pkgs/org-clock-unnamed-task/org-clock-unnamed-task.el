1addde930bfe886bbb5c8b9cfca2668ee5a80049
Sharad Pratap <sh4r4d@gmail.com>
2018-09-11 18:18:34 +0530

    Scripted auto-commit on change (2018-09-11 18:18:34) by gitwatch.sh

--- xemacs/elpa/pkgs/org-clock-unnamed-task/org-clock-unnamed-task.el
+++ xemacs/elpa/pkgs/org-clock-unnamed-task/org-clock-unnamed-task.el
@@ -133,9 +133,9 @@
      (lotus-org-create-unnamed-task file parent-task)
      (org-entry-put nil "Effort" "10")
      ;; BUG: It is clocking to last unfinished task
-     (org-clock-clock-in)
+     (org-clock-clock-in (lotus-org-unnamed-task-clock-marker (mark-marker)))
      ;; (org-clock-in)
-     (lotus-org-unnamed-task-clock-marker (mark-marker)))))
+     )))
 
 (when nil
 
