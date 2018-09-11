5d221a33a5c8aa952018e7c4f16cbb6e70d62979
Sharad Pratap <sh4r4d@gmail.com>
2018-09-12 02:21:29 +0530

    Scripted auto-commit on change (2018-09-12 02:21:29) by gitwatch.sh

--- spacemacs-mycontribs/+local-session/lotus-orgmode/config.el
+++ spacemacs-mycontribs/+local-session/lotus-orgmode/config.el
@@ -297,11 +297,12 @@
 
     (progn
       (add-to-task-current-party-change-hook
-       #'(lambda ()
-           (let* ((party-base-dir (task-party-base-dir))
-                  (start-file (expand-file-name "start.org" party-base-dir)))
-             (setq
-              org-agenda-files (occ-included-files))))))))
+       (occ-run-with-global-tsk-collection
+        #'(lambda ()
+            (let* ((party-base-dir (task-party-base-dir))
+                   (start-file (expand-file-name "start.org" party-base-dir)))
+              (setq
+               org-agenda-files (occ-included-files)))))))))
 
 (defun lotus-orgmode-config/post-init-org-agenda ()
   (progn
