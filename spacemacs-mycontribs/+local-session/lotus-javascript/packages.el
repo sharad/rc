c5882521bcefbfb89226903e86355fb4a8b9acaf
Sharad Pratap <sh4r4d@gmail.com>
2018-09-14 00:25:44 +0530

    Scripted auto-commit on change (2018-09-14 00:25:44) by gitwatch.sh

--- spacemacs-mycontribs/+local-session/lotus-javascript/packages.el
+++ spacemacs-mycontribs/+local-session/lotus-javascript/packages.el
@@ -429,7 +429,7 @@ Each entry is either:
         (add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode)))))
 
 
-(defun lotus-LAYER/init-sws-mode ()
+(defun lotus-javascript/init-sws-mode ()
   (use-package ample
       :defer t
       :config
