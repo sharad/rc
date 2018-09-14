d5992a0f904a94c12828722f946fdec9b7976ab1
Sharad Pratap <sh4r4d@gmail.com>
2018-09-15 00:58:44 +0530

    Scripted auto-commit on change (2018-09-15 00:58:44) by gitwatch.sh

--- spacemacs-mycontribs/+local-session/lotus-appt/packages.el
+++ spacemacs-mycontribs/+local-session/lotus-appt/packages.el
@@ -82,7 +82,8 @@ Each entry is either:
                        ;; http://www.emacswiki.org/emacs/AppointmentMode
                        (if (not running-xemacs)
                            (appt-activate 1); use (appt-activate 1) for GNU Emacs
-                         (appt-initialize)) ; XEmacs))))))))
+                         (appt-initialize)) ; XEmacs
+                       ))))))
         (progn
 
           (setq appt-msg-countdown-list '(10 5 1) ; XEmacs
