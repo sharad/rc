8b6a6bb17fdcaaf7ea04c15bad380af2abf4f1b2
Sharad Pratap <sh4r4d@gmail.com>
2018-09-15 01:39:02 +0530

    Scripted auto-commit on change (2018-09-15 01:39:02) by gitwatch.sh

--- spacemacs-mycontribs/+local-session/lotus-schedule/packages.el
+++ spacemacs-mycontribs/+local-session/lotus-schedule/packages.el
@@ -176,8 +176,7 @@ Each entry is either:
                           (content-misc-dir local-diary-relative-path))
                          (local-diary-output-directory
                           (content-misc-publishing-dir local-diary-relative-path)))
-                    )
-                  ))
+                    )))
               )
 
           )
