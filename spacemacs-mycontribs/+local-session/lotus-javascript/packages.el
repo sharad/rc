348703de1562642d3c96a77eb814c1b833997ace
Sharad Pratap <sh4r4d@gmail.com>
2018-09-14 16:13:53 +0530

    Scripted auto-commit on change (2018-09-14 16:13:53) by gitwatch.sh

--- spacemacs-mycontribs/+local-session/lotus-javascript/packages.el
+++ spacemacs-mycontribs/+local-session/lotus-javascript/packages.el
@@ -452,7 +452,24 @@ Each entry is either:
       :config
       (progn
         (progn
+          (use-package xref
+              :defer t
+              :config
+              (progn
+                (progn
+                  (use-package js2-mode
+                      :defer t
+                      :config
+                      (progn
+                        (progn
+                          ;; https://github.com/nicolaspetton/xref-js2
+                          ;; If you use js2-mode, M-. will be bound by js2, you might want to unbind it:
+                          (define-key js2-mode-map (kbd "M-.") nil)
+                          ;; Then you need to add the xref backend:
+                          (add-hook 'js2-mode-hook
+                                    #'(lambda ()
+                                        (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
 
-          ))))
+                          ))))))))))
 
 ;;; packages.el ends here
