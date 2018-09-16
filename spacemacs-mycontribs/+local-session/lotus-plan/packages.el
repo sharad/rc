e4a4fcbe46a09abbdffb355e1545be143fc649ce
Sharad Pratap <sh4r4d@gmail.com>
2018-09-16 18:53:57 +0530

    Scripted auto-commit on change (2018-09-16 18:53:56) by gitwatch.sh

--- spacemacs-mycontribs/+local-session/lotus-plan/packages.el
+++ spacemacs-mycontribs/+local-session/lotus-plan/packages.el
@@ -149,7 +149,7 @@ Each entry is either:
               (let* ((local-planner-relative-path "web/site/wiki/Organize/plan/Plans")
                      (local-planner-directory
                       ;; (expand-file-name local-planner-relative-path *muse-top-dir*)
-                       (content-muse-dir local-planner-relative-path))
+                      (publishing-created-contents-path 'muse local-planner-relative-path))
                      (local-planner-output-directory
                       ;; (expand-file-name "planner-xhtml" (expand-file-name local-planner-relative-path *muse-generated-top-dir*))
                       (content-muse-publishing-dir
