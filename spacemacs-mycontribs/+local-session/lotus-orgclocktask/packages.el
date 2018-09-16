1ea5ea3d33539b276f1e32d059b8e5898bb5b8d8
Sharad Pratap <sh4r4d@gmail.com>
2018-09-16 22:30:27 +0530

    Scripted auto-commit on change (2018-09-16 22:30:27) by gitwatch.sh

--- spacemacs-mycontribs/+local-session/lotus-orgclocktask/packages.el
+++ spacemacs-mycontribs/+local-session/lotus-orgclocktask/packages.el
@@ -370,9 +370,10 @@ Each entry is either:
                            (when (task-current-party)
                              (let ((monitor-dir (task-party-dir)))
                                (if (file-directory-p monitor-dir)
-                                   (progn
-                                     (org-clock-monitor-files-set-from-dir monitor-dir)
-                                     (org-clock-work-day-mode-line-add t))
+                                   (if (fboundp 'org-clock-monitor-files-set-from-dir)
+                                    (progn
+                                      (org-clock-monitor-files-set-from-dir monitor-dir)
+                                      (org-clock-work-day-mode-line-add t)))
                                  (message "[4]org monitor dir %s not exists." monitor-dir)))))
                          (org-clock-work-day-mode-line-add t))
                      ((error) (message "Error: %s" err)))
