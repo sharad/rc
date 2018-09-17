0446b7e93db060648554de02e21cf5e13231c922
Sharad Pratap <sh4r4d@gmail.com>
2018-09-17 21:10:57 +0530

    Scripted auto-commit on change (2018-09-17 21:10:57) by gitwatch.sh

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -79,7 +79,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(elscreen)
+   dotspacemacs-additional-packages '(lotus-utils elscreen)
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
