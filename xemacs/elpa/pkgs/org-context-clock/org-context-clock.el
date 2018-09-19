cb72f2f7337a8163326087c7bd38f744baf9b1fe
Sharad Pratap <sh4r4d@gmail.com>
2018-09-19 23:32:27 +0530

    Scripted auto-commit on change (2018-09-19 23:32:27) by gitwatch.sh

--- xemacs/elpa/pkgs/org-context-clock/org-context-clock.el
+++ xemacs/elpa/pkgs/org-context-clock/org-context-clock.el
@@ -28,3 +28,87 @@
 
 ;;; Code:
 ;; Preamble:1 ends here
+
+;; [[file:~/.xemacs/elpa/pkgs/org-context-clock/org-context-clock.org::*Preamble][Preamble:2]]
+(defgroup org-context-clock nil
+  "Emacs Org Context Clocking."
+  :tag "Org Clock"
+  :group 'org-progress)
+;; Preamble:2 ends here
+
+;; Required libraries
+
+
+;; [[file:~/.xemacs/elpa/pkgs/org-context-clock/org-context-clock.org::*Required%20libraries][Required libraries:1]]
+(require 'org-clock)
+
+ (require 'timer-utils-lotus)
+ (eval-when-compile
+   (require 'timer-utils-lotus))
+ (require 'org-misc-utils-lotus)
+ (eval-when-compile
+   (require 'org-misc-utils-lotus))
+ (require 'lotus-misc-utils)
+ (eval-when-compile
+   (require 'lotus-misc-utils))
+
+(require 'org-onchange)
+;; Required libraries:1 ends here
+
+;; It is divided into multiple files for different functionality
+
+
+;; [[file:~/.xemacs/elpa/pkgs/org-context-clock/org-context-clock.org::*It%20is%20divided%20into%20multiple%20files%20for%20different%20functionality][It is divided into multiple files for different functionality:1]]
+(require 'org-context-clock-api)
+(require 'org-context-clock-api-list) ;; "org tasks access api for list org"
+(require 'org-context-clock-api-recursive) ;; "org tasks access api for recursive task"
+(require 'org-context-clock-api-interaction) ;; "Interactive utitlity API's for adding root subtree etc" ;; "org tasks clocking's API"
+(require 'org-context-clock-assoc-predicate) ;; "org tasks associated to context predicate functions"
+(require 'org-context-clock-assoc-rank) ;; "Org tasks associated to context rank functions"
+(require 'org-context-clock-assoc-key) ;; "org tasks associated to context key functions on recursive taskinfos"
+;; It is divided into multiple files for different functionality:1 ends here
+
+;; Global variables
+
+;; [[file:~/.xemacs/elpa/pkgs/org-context-clock/org-context-clock.org::*Global%20variables][Global variables:1]]
+(defvar *org-context-clock-task-current-context*  nil)
+(defvar *org-context-clock-task-previous-context* nil)
+(defvar *org-context-clock-clocked-dyntaskpl-context-history*  nil)
+(defvar *org-context-clock-task-current-context-time-interval* 7)
+(defvar *org-context-clock-last-buffer-select-time* (current-time))
+(defvar *org-context-clock-buffer-select-timer* nil)
+(defvar *org-context-clock-update-current-context-msg* "")
+;; (defvar org-context-clock-api-name :predicate "API")
+(defvar org-context-clock-access-api-name :recursive "Aceess API")
+(defvar org-context-clock-assoc-api-name :keys "Assoc API")
+(defvar org-context-clock-api-dyntaskpl-print                  (org-context-clock-access-api-get org-context-clock-access-api-name :dyntaskplprint))
+
+;; deprecated
+(defvar org-context-clock-api-dyntaskpls-associated-to-context (org-context-clock-access-api-get org-context-clock-access-api-name :dyntaskpls))
+(defvar org-context-clock-api-tasks-associated-to-context      (org-context-clock-access-api-get org-context-clock-access-api-name :tasks))
+(defvar org-context-clock-build-dyntaskpl                      (org-context-clock-access-api-get org-context-clock-access-api-name :dyntaskpl))
+(defvar org-context-clock-matching-dyntaskpls                  (org-context-clock-access-api-get org-context-clock-access-api-name :dyntaskpls))
+(defvar org-context-clock-matching-tasks                       (org-context-clock-access-api-get org-context-clock-access-api-name :tasks))
+(defvar org-context-clock-api-task-associated-to-context-p     (org-context-clock-assoc-api-get  org-context-clock-assoc-api-name :taskp))
+(defvar org-context-clock-api-task-update-tasks                (org-context-clock-access-api-get org-context-clock-access-api-name :update))
+(defvar org-context-clock-api-task-update-files                (org-context-clock-access-api-get org-context-clock-access-api-name :files))
+;; Global variables:1 ends here
+
+;; Simple function
+
+
+;; [[file:~/.xemacs/elpa/pkgs/org-context-clock/org-context-clock.org::*Simple%20function][Simple function:1]]
+(defun custom-plist-keys (in-plist)
+  (if (null in-plist)
+      in-plist
+      (cons (car in-plist) (custom-plist-keys (cddr in-plist)))))
+;; Simple function:1 ends here
+
+;; Disable for some time
+
+;; [[file:~/.xemacs/elpa/pkgs/org-context-clock/org-context-clock.org::*Disable%20for%20some%20time][Disable for some time:1]]
+(defun org-context-clock-disable-for (time)
+  "Disable context clocking for TIME period."
+  ;; Implement
+  )
+;; Disable for some time:1 ends here
