de4910f270f8bd187c745d9343be3d09cbcb7204
Sharad Pratap <sh4r4d@gmail.com>
2018-09-27 15:07:30 +0530

    Scripted auto-commit on change (2018-09-27 15:07:29) by gitwatch.sh

--- binx/setup/setup-user.sh
+++ binx/setup/setup-user.sh
@@ -443,6 +443,7 @@ function setup_git_repos()
         running git -c core.sshCommand="$GIT_SSH_OPTION" clone --recursive  git@github.com:sharad/userorg.git ~/${RESOURCEPATH}/userorg
     else
         running git -c core.sshCommand="$GIT_SSH_OPTION" -C ~/${RESOURCEPATH}/userorg pull --rebase
+        # running git -c core.sshCommand="$GIT_SSH_OPTION" -C ~/${RESOURCEPATH}/userorg submodule foreach git -c core.sshCommand="$GIT_SSH_OPTION" pull --rebase
         running git -c core.sshCommand="$GIT_SSH_OPTION" -C ~/${RESOURCEPATH}/userorg submodule foreach git -c core.sshCommand="$GIT_SSH_OPTION" pull --rebase
         # git -c core.sshCommand="$GIT_SSH_OPTION" -C ~/.repos/git submodule update --remote
     fi
