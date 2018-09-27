e0f61184361130827df4b802c96a466bf5fbb437
Sharad Pratap <sh4r4d@gmail.com>
2018-09-27 21:05:39 +0530

    correction

--- binx/setup/setup-user.sh
+++ binx/setup/setup-user.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 DEBUG=1
+export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
 
 SSH_KEY_DUMP=$1
 TMPDIR=~/setuptmp
+
 if [ -r ~/.ssh/authorized_keys ]
 then
     # GIT_SSH_OPTION="ssh -o UserKnownHostsFile=~/.ssh/authorized_keys -o StrictHostKeyChecking=yes"
