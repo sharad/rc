a1d65bd883371f36e8a2af1eae7278f54e544a8a
Sharad Pratap <sh4r4d@gmail.com>
2018-10-12 16:36:23 +0530

    correction

--- binx/sec/privacy/conn-one.sh
+++ binx/sec/privacy/conn-one.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+
+
+
+
+nmcli con down "nlisp 5"
+nmcli con up id "Wired connection";
+nmcli -a dev wifi rescan
+nmcli con up id Forti-corp-wpa2psk;
+
+
+
+
+nmcli con down id "Wired connection";
+nmcli con down id Forti-corp-wpa2psk;
+nmcli -a dev wifi rescan
+nmcli -a dev wifi connect nlisp
