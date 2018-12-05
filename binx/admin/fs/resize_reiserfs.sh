c3c67c66dc8cf2109c95c4323bbab3c551beb789
Sharad Pratap <sh4r4d _at_ _G-mail_>
2018-12-06 03:39:49 +0530

    correction

---
+++ binx/admin/fs/resize_reiserfs.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+syncimap -d
+
+LVPART=/dev/mapper/vgres01-lvres01
+
+
+pkill offlineimap
+sudo systemctl stop dovecot.service
+sudo systemctl stop postfix.service
+
+sleep 3s
+
+if pgrep offlineimap
+then
+    echo sleep 30s
+    sleep 30s
+    echo pkill -9 offlineimap
+    pkill -9 offlineimap
+fi
+
+if sudo umount $LVPART
+then
+    echo df $LVPART
+    df $LVPART
+    echo sleep 10s
+    sleep 10s
+    sudo reiserfsck --check $LVPART
+    sudo lvresize -L +2G $LVPART
+    sudo resize_reiserfs $LVPART
+else
+    sudo lsod $LVPART
+fi
+
+if sudo mount $LVPART
+then
+    sudo systemctl start dovecot.service
+    sudo systemctl start postfix.service
+    syncimap -r
+fi
