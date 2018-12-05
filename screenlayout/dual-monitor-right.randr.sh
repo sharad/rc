bcf55f59f929366317be2dd00bd948bfc9f2326e
Sharad Pratap <sh4r4d _at_ _G-mail_>
2018-12-05 23:53:10 +0530

    correction

--- screenlayout/dual-monitor-right.randr.sh
+++ screenlayout/dual-monitor-right.randr.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xrandr \
-    --output VGA-1 --mode 1366x768 --pos 0x0 --rotate normal \
-    --output LVDS-1 --primary --mode 1600x900 --pos 1366x0 --rotate normal \
+    --output VGA-1 --primary --mode 1366x768 --pos 0x0 --rotate normal \
+    --output LVDS-1 --mode 1600x900 --pos 1366x0 --rotate normal \
     --output HDMI-3 --off \
     --output HDMI-2 --off \
     --output HDMI-1 --off \
