1321e3aec72a3fc3aad2fac62798c436f52ce9e8
Sharad Pratap <sh4r4d _at_ _G-mail_>
2019-03-08 21:39:37 +0530

    correction

---
+++ xemacs/elpa/pkgs/org-capture+/org-capture+-helm.el
@@ -0,0 +1,66 @@
+;;; org-capture+-helm.el --- org capture+ helm       -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Sharad
+
+;; Author: Sharad <sh4r4d@gmail.com>
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(provide 'org-capture+-helm)
+
+
+(require 'helm)
+
+
+(setq org-capture+-helm-templates-alist
+  '(("TODO"
+     "* TODO %? %^g\n %i\n [%a]\n"
+     "* MILESTONE %? %^g\n %i\n [%a]\n")
+    ("MEETING"
+     "* MEETING %? %^g\n %i\n [%a]\n")))
+
+
+(defun org-capture+-build-helm-template-source (name attrib-list &rest templates)
+  `((name . ,name)
+    (multiline)
+    (candidates ,@templates)
+    ,@attrib-list))
+
+(defun org-capture+-build-helm-template-sources (attrib-list alist)
+  (mapcar
+   #'(lambda (e)
+       (apply #'org-capture+-build-helm-template-source
+        (car e)
+        attrib-list
+        (cdr e)))
+   alist))
+
+(defun org-capture+-helm-select-template (&optional attrib-list alist)
+  (let ((attrib-list (or attrib-list '((action . identity))))
+        (alist       (or alist org-capture+-helm-templates-alist)))
+    (helm :sources
+          (org-capture+-build-helm-template-sources attrib-list alist))))
+
+(org-capture+-helm-select-template)
+
+
+
+;;; org-capture+-helm.el ends here
