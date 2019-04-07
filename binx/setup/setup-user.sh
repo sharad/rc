1873f4568691a5ae577d792803da538a3723a8a9
Sharad Pratap <sh4r4d _at_ _G-mail_>
2019-04-07 22:46:19 +0530

    correction

--- binx/setup/setup-user.sh
+++ binx/setup/setup-user.sh
@@ -74,7 +74,7 @@ DEB_EXTRA_PKG3_UTILS="system-config-lvm lvmeject adcli partclone gpodder paralle
 # https://www.cyberciti.biz/faq/removing-password-from-pdf-on-linux/
 DEB_PKG_DEV="valgrind libxml2-dev gjs seed-webkit2 xpdf-utils ghostscript pdftk qpdf editorconfig elpa-editorconfig vim-editorconfig pythin-editorconfig python3-editorconfig"
 DEB_PKG_SYSTEM="cpuid inxi x11-xserver-utils autorandr arandr bluez bluez-tools redshift daemontools god circus software-properties-common at hibernate ps-watcher daemonfs daemonize daemon slop scrot"
-DEB_PKG_TOOL="atool fpgatools patool"
+DEB_PKG_TOOL="atool fpgatools patool usbmount"
 DEB_PKG_TOOL_TEST="cyrus-clients swaks im namazu2-index-tools prayer-accountd prayer"
 DEB_SYS_PKG1="duc baobab agedu tpb daemontools sysstat isag dos2unix powermanagement-interface grub2-splashimages grub2-themes-ubuntu-mate offlineimap libsecret-tools"
 # https://linuxconfig.org/fetch-stock-quotes-with-perl-finance-quote-module
