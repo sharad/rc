5fc38e5055edd8e756a2fa057b07390f7dc05022
Sharad Pratap <110339+sharad@users.noreply.github.com>
2019-10-01 13:47:59 +0530

    correction commit

---
+++ xemacs/elpa/pkgs/code-snippet/code-snippet.el
@@ -0,0 +1,56 @@
+;;; code-snippet.el --- code snippet                 -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  s
+
+;; Author: s <sh4r4d@gmail.com>
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(provide 'code-snippet)
+
+
+;; check out
+;; (c-mark-function)
+
+;; copy code like below
+
+;; Mgr::Function() {
+
+;;   [...]
+
+;; [1] https://domain/path/file.cpp#L12479
+
+;;             SetTimeout(TIME_SECS);
+;;             uint32_t listsz = m_list.size();
+;;             list.insert(pair<uint16_t, msga*>
+;;                                                      (xyz, test));
+
+;;   [...]
+
+;; [2] https://domain/path/file.cpp#L12726
+
+;;    return Send();
+
+;; }
+
+
+
+;;; code-snippet.el ends here
