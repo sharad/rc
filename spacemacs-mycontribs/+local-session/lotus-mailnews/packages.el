53ec1ad330f510d3c420db83d640604da6e53c9f
Sharad Pratap <110339+sharad@users.noreply.github.com>
2019-10-26 00:41:09 +0530

    correction

--- spacemacs-mycontribs/+local-session/lotus-mailnews/packages.el
+++ spacemacs-mycontribs/+local-session/lotus-mailnews/packages.el
@@ -37,10 +37,21 @@
 (defconst lotus-mailnews-packages
   '(
     (gnus :location local)
+    bbdb
     lsdb
+    shimbun
+    notmuch
     gnus-win
     gnus-sum
     gnus-msg
+    gnus-pers
+    gnus-namazu
+    gnus-demon
+    gnus-dired
+    message
+    sendmail
+    dbus
+    mailcrypt
     )
   "The list of Lisp packages required by the lotus-mailnews layer.
 
@@ -76,8 +87,30 @@ Each entry is either:
     :config
     (progn
       (progn
-        (progn
-          (setq gnus-init-file "~/.gnus.el"))
+        (gnus-delay-initialize))
+      (progn
+        (setq gnus-select-method '(nntp "news.gmane.org"))
+
+        (add-to-list
+         'gnus-secondary-select-methods
+         '(nnimap "localhost"
+                  (nnimap-address "localhost")
+                  ;; (nnimap-server-port 993)
+                  ;; (nnimap-server-port 443)
+                  (nnimap-server-port 143)
+                  (nnimap-stream network)
+                  (nnimap-authenticator login)
+                  (nnimap-authinfo-file "~/.authinfo.gpg")
+                  (nnir-search-engin imap)))
+
+        (add-to-list
+         'gnus-secondary-select-methods
+         `(nnvirtual
+           ,(if (equal (system-name) office-host-name)
+                "Office\\.INBOX\\|Office\\.sent-mail"
+              "Gmail\\.INBOX\\|Gmail\\.sent-mail"))))
+      (progn
+        (setq gnus-init-file "~/.gnus.el")
         (make-directory (expand-file-name ".cache/autoconfig/gnus/" user-emacs-directory) t)
         (setq
          gnus-home-directory (expand-file-name "autoconfig/gnus/" user-emacs-directory))
@@ -120,6 +153,52 @@ Each entry is either:
          'gnus-article-prepare-hook
          'gnus-treat-mail-gravatar)))))
 
+(defun lotus-mailnews/post-init-bbdb ()
+  (use-package bbdb
+    :defer t
+    :config
+    (progn
+      (progn
+        (unless (fboundp 'bbdb-insinuate-gnus)
+          (defun bbdb-insinuate-gnus ()
+            (bbdb-initialize 'gnus)))
+        (add-hook 'gnus-startup-hook 'bbdb-insinuate-gnus))
+
+      (progn
+        (setq bbdb-file (expand-file-name "bbdb/bbdb" "~/.emacs.d/.cache/autoconfig"))
+
+        (defun bbdb/gnus-pop-up-bbdb-buffer-for-some-time ()
+          (if (functionp 'bbdb/gnus-pop-up-bbdb-buffer)
+              (progn
+                (bbdb/gnus-pop-up-bbdb-buffer)
+                ;; (with-selected-window (get-buffer-window gnus-article-buffer)
+                ;;   (gnus-summary-goto-subject (cdr gnus-article-current)))
+                (let ((win-bbdb (get-buffer-window "*BBDB*")))
+                  (when win-bbdb
+                    ;; (run-at-time "4 sec" nil #'delete-window w))))
+                    (run-at-time "4 sec" nil #'(lambda (w)
+                                                 (if (and
+                                                      (windowp w)
+                                                      (window-valid-p w))
+                                                     ;; (old-delete-window w)
+                                                     (progn
+                                                       (delete-window w)
+                                                       (message "deleted %s window" w))))
+                                 win-bbdb))))
+            (message "#'bbdb/gnus-pop-up-bbdb-buffer is not present in bbdb3 use some other function for popup.")))
+
+        (define-key gnus-summary-mode-map (kbd "s-c s-v")  'bbdb/gnus-pop-up-bbdb-buffer)
+
+        (setq bbdb-use-pop-up t
+              bbdb-save-db-timeout 0) ;; I want it
+        (remove-hook 'gnus-article-prepare-hook 'bbdb/gnus-pop-up-bbdb-buffer)
+        (add-hook 'gnus-article-prepare-hook 'bbdb/gnus-pop-up-bbdb-buffer-for-some-time)
+
+        (defun toggle-bbdb-use-pop-up ()
+          (interactive)
+          (setq
+           bbdb-use-pop-up (not bbdb-use-pop-up)))))))
+
 (defun lotus-mailnews/post-init-lsdb ()
   (use-package lsdb
     :defer t
@@ -127,6 +206,44 @@ Each entry is either:
     (progn
       (progn
         (setq lsdb-file (lotus-cache-file "lsdb/lsdb"))))))
+
+(defun lotus-mailnews/init-shimbun ()
+  (use-package sb-rss-blogs ;; shimbun
+    :defer t
+    :config
+    (progn
+      (progn
+        (add-to-list 'shimbun-rss-blogs-group-url-regexp
+                     '("OSNews" "http://www.osnews.com/files/recent.xml"))
+        (add-to-list 'shimbun-rss-blogs-group-url-regexp
+                     '("LinuxToday" "http://feedproxy.google.com/linuxtoday/linux"))
+
+        (add-to-list 'shimbun-rss-blogs-group-url-regexp
+                     '("NDTV" "http://feeds2.feedburner.com/NdtvNews-TopStories")))
+      (progn
+        (setq shimbun-atom-hash-group-path-alist
+              '(("LinuxToday" "http://feedproxy.google.com/linuxtoday/linux" t)
+                ("OSNews" "http://www.osnews.com/files/recent.xml" t)
+                ("PlanetEmacsen" "http://planet.emacsen.org/atom.xml" t)
+                ("Stackoverflow: Emacs" "http://stackoverflow.com/feeds/tag/emacs" t))
+
+              shimbun-rss-hash-group-path-alist
+              '(("LinuxToday" "http://feedproxy.google.com/linuxtoday/linux" t)
+                ("OSNews" "http://www.osnews.com/files/recent.xml" t)
+                ("Stackoverflow: Emacs" "http://stackoverflow.com/feeds/tag/emacs" t)))))))
+
+(defun lotus-mailnews/init-notmuch ()
+  (use-package notmuch-address
+    :defer t
+    :config
+    (progn
+      (progn
+        (setq notmuch-fcc-dirs nil))
+      (progn
+        ;; http://notmuchmail.org/emacstips/
+        (setq notmuch-address-command (expand-file-name "notmuch-addrlookup" "~/bin"))
+        (if (file-exists-p notmuch-address-command)
+            (notmuch-address-message-insinuate))))))
 
 (defun lotus-mailnews/init-gnus-win ()
   (use-package gnus-win
@@ -467,23 +584,42 @@ Each entry is either:
         ;;   (with-current-buffer gnus-article-buffer
         ;;     (message-fetch-field "to")))
 
-
-
-
-
-
-
-
-
-
-
         ))))
 
 (defun lotus-mailnews/init-gnus-art ()
-  (use-package lsdb
+  (use-package gnus-art
     :defer t
     :config
     (progn
+      (progn
+        (add-hook 'gnus-article-display-hook 'gnus-article-highlight-citation))
+      (progn
+        (setq gnus-signature-separator
+              '("^-- $"
+                "^-- *$"
+                "^-------*$"
+                "^ *--------*$"
+                "^________*$"
+                "^========*$")))
+      (progn
+        (gnus-start-date-timer))
+      (progn
+        (setq gnus-article-date-lapsed-new-header t)
+        (add-hook 'gnus-part-display-hook 'gnus-article-date-lapsed)
+        (add-hook 'gnus-part-display-hook 'gnus-article-date-local)
+
+        (add-hook 'gnus-article-prepare-hook
+                  '(lambda ()
+                     ;; 	     (gnus-article-de-quoted-unreadable)
+                     (gnus-article-emphasize)
+                     (gnus-article-hide-boring-headers)
+                     (gnus-article-hide-headers-if-wanted)
+                     ;; 	     (gnus-article-hide-pgp)
+                     (gnus-article-highlight)
+                     (gnus-article-highlight-citation)
+                     (gnus-article-date-lapsed)
+                     (gnus-article-date-local)              ; will actually convert timestamp from other timezones to yours
+                     (gnus-article-strip-trailing-space))))
       (progn
         (setq
          gnus-treat-body-boundary 'head
@@ -524,7 +660,6 @@ Each entry is either:
                   "{ echo \
 '/* Format_version=1, Width=48, Height=48, Depth=1, Valid_bits_per_item=16 */'\
 ; uncompface; } | icontopbm | display -"))))
-
       (progn
         (setq gnus-visible-headers
               '(
@@ -569,5 +704,245 @@ Each entry is either:
       (progn
         (setq
          nnmail-extra-headers '(To Newsgroups Content-Type Date))))))
+
+(defun lotus-mailnews/init-gnus-pers ()
+  (use-package gnus-pers
+    :defer t
+    :config
+    (progn
+      (progn
+        (defvar gnus-personality-activate nil "")
+        (gnus-personality-init/sharad)))))
+
+(defun lotus-mailnews/init-gnus-namazu ()
+  (use-package gnus-namazu
+    :defer t
+    :config
+    (progn
+      (progn
+        (gnus-namazu-insinuate)
+
+        (setq gnus-namazu-index-update-interval nil)
+
+        (defun xsteve-gnus-namazu-update-all-indices ()
+          (interactive)
+          (gnus-namazu-update-all-indices t))
+
+        (defun xsteve-gnus-update-namazu-index ()
+          (run-at-time "6:00am" nil 'xsteve-gnus-namazu-update-all-indices)))
+      (progn
+        (use-package midnight
+          :defer t
+          :config
+          (progn
+            (add-hook 'midnight-hook 'xsteve-gnus-update-namazu-index)))))))
+
+(defun lotus-mailnews/init-gnus-dired ()
+  (use-package gnus-dired
+    :defer t
+    :config
+    (progn
+      (progn
+        (add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)))))
+
+
+
+(defun lotus-mailnews/init-gnus-daemon ()
+  (use-package gnus-daemon
+    :defer t
+    :config
+    (progn
+      (setq gnus-use-demon t)
+      (add-hook 'gnus-group-mode-hook 'gnus-demon-init)
+      (add-hook 'gnus-exit-gnus-hook 'gnus-demon-cancel)
+      ;; Sort threads by the date of the root node.
+      (setq gnus-thread-sort-functions `(gnus-thread-sort-by-date))
+      ;; Initialize the Gnus daemon, check new mail every six minutes.
+      ;; (gnus-demon-add-handler 'gnus-demon-scan-mail-and-news 1 nil))
+      ;; (gnus-demon-add-handler 'gnus-demon-scan-mail-and-news-now 2 nil)
+      (gnus-demon-add-handler 'gnus-demon-scan-mail-and-news-now 10 10)
+      (gnus-demon-add-handler '(lambda () (gnus-demon-scan-mail-and-news-now 6)) 22 20))))
+
+(defun lotus-mailnews/init-message ()
+  (use-package message
+    :defer t
+    :config
+    (progn
+      (progn
+        (setq
+         gnus-message-archive-method '(nnimap "localhost")
+         gnus-message-archive-group        ;even I have handled it in gnus-posting-style
+         #'(lambda (group)
+             (append
+              (list "sent")
+              (if (message-news-p)
+                  '("sent-news")
+                `("sent-mail"
+                  ,@(if (member (system-name) office-host-names)
+                        '("Office.Meru.Sent Items" "Office.Fortinet.Sent Items"))))
+              (list (format-time-string "sent.%Y-%m"))))))
+      (progn
+        (setq message-alternative-emails
+              (regexp-opt
+               (list
+                email-addr
+                office-email
+                office-fortinet-email
+                office-meru-email)))
+        (setq message-dont-reply-to-names
+              (append
+               (list
+                user-mail-address
+                email-addr
+                office-email
+                office-fortinet-email
+                office-meru-email)
+               user-other-email-addresses)))
+      (progn
+        (setq gnus-gcc-externalize-attachments nil)
+        (add-hook 'message-send-hook 'check-attachments-attached))
+      (progn
+        ;;{{ XSteve, insert Hi Hello Name
+
+        (setq message-citation-line-function 'message-insert-formatted-citation-line
+              message-cite-function 'message-cite-original-without-signature)
+        (add-hook 'message-setup-hook 'xsteve-message-citation t)
+        ;;
+
+        (defun sharad-message-citation-delete ()
+          "Delete Hi."
+          (message-goto-body)
+          (search-forward-regexp "Hi")
+          (move-beginning-of-line 1)
+          (if (looking-at "Hi")
+              (kill-line))))
+      (progn
+        (define-key message-mode-map [f6] 'xsteve-message-citation-toggle))
+      (progn
+        (add-hook 'message-sent-hook 'gnus-score-followup-article)
+        (add-hook 'message-sent-hook 'gnus-score-followup-thread))
+
+      (progn
+        (setq message-cite-reply-above nil
+              message-cite-reply-above t
+              ;; http://emacsworld.blogspot.in/2011/11/gnus-tip-customising-position-of-point.html
+              message-cite-reply-position 'traditional
+              message-cite-reply-position 'above))
+
+      (progn
+        (use-package ispell
+          :defer t
+          :config
+          (progn
+            ;; Ispell.el assumes you use ispell, if you choose aspell say
+            ;; (setq ispell-program-name "timeout -k 12 10 aspell")
+            (setq ispell-program-name "aspell")
+            ;; in your Emacs configuration file.  If you want your outgoing
+            ;; messages to be spell-checked, say
+            (add-hook 'message-send-hook 'ispell-message)
+            ;; In your ~/.gnus.el, if you prefer on-the-fly spell-checking say
+            (add-hook 'message-mode-hook (lambda () (flyspell-mode 1))))))
+
+      (progn
+        (use-package footnote
+          :defer t
+          :config
+          (progn
+            (add-hook 'message-mode-hook (lambda () (footnote-mode 1))))))
+      (progn
+        (add-hook 'message-mode-hook ;          'turn-on-auto-fill)
+                  '(lambda ()
+                     (turn-on-auto-fill)
+                     (setq fill-column 70)))
+        (setq message-generate-headers-first t)
+        (setq message-kill-buffer-on-exit t)
+        ;;{{ http://www.gnus.org/manual/gnus_401.html
+        (setq gnus-confirm-mail-reply-to-news t)
+        (setq message-user-fqdn (concat "personal.machine.of." myshortname ".com")))
+      (progn
+        (defvar *use-msmtp-for-senmail* nil "msmtp to use")
+
+        (setq *use-msmtp-for-senmail* (equal (system-name) "asfsdspratap"))
+
+        (if *use-msmtp-for-senmail* ;; where I am using msmtp
+            (setq  ;; for msmtp
+             ;; see http://www.gnus.org/manual/message_36.html
+             message-sendmail-f-is-evil t
+             message-sendmail-envelope-from nil)
+          (setq
+           ;; see http://www.gnus.org/manual/message_36.html
+           message-sendmail-f-is-evil nil
+           message-sendmail-envelope-from 'header))))))
+
+(defun lotus-mailnews/init-sendmail ()
+  (use-package sendmail
+    :defer t
+    :config
+    (progn
+      (progn
+        (setq mail-specify-envelope-from t
+              mail-envelope-from 'header))
+      (progn
+        (setq send-mail-function 'sendmail-send-it)))))
+
+(defun lotus-mailnews/init-dbus ()
+  (use-package dbus
+    :defer t
+    :config
+    (progn
+      (defvar gnus-nm-dbus-registration nil "dsfdsf")
+      (defvar gnus-nm-connected-hook nil
+        "Functions to run when network is connected.")
+      (defvar gnus-nm-disconnected-hook nil
+        "Functions to run when network is disconnected.")
+      (setq gnus-nm-dbus-registration nil)
+
+      ;; Add hooks for plugging/unplugging on network state change:
+      (add-hook 'gnus-nm-connected-hook    'gnus-nm-agent-plug)
+      (add-hook 'gnus-nm-connected-hook    'gnus-group-send-queue)
+      (add-hook 'gnus-nm-disconnected-hook 'gnus-nm-agent-unplug)
+      ;; Add hooks for enabling/disabling integration on startup/shutdown:
+      (add-hook 'gnus-started-hook   'gnus-nm-enable)
+      (add-hook 'gnus-exit-gnus-hook 'gnus-nm-disable))))
+
+(defun lotus-mailnews/init-mailcrypt ()
+  (use-package mailcrypt
+    :defer t
+    :commands (mc-install-read-mode mc-install-write-mode)
+    :config
+    (progn
+      ;; http://www.suse.de/~garloff/Writings/mutt_gpg/node18.html
+      (mc-setversion "gpg")
+      (add-hook 'gnus-summary-mode-hook 'mc-install-read-mode)
+      (add-hook 'message-mode-hook 'mc-install-write-mode)
+      (add-hook 'news-reply-mode-hook 'mc-install-write-mode)
+
+      ;; Next time you start Gnus, you get a menu called ¨Mailcrypt¨ which
+      ;; gives you an easy access to GnuPG.
+
+      ;; Some variable settings which might be helpful are:
+
+      ;; Use the pgp2 compatibility wrapper
+      (setq mc-gpg-path "/usr/bin/gpg-compat")
+
+      ;; If you have more than one key, specify the one to use
+      (setq mc-gpg-user-id "0x12345678")
+
+      ;; Always sign encrypted messages
+      (setq mc-pgp-always-sign t)
+
+      ;; How long should mailcrypt remember your passphrase
+      (setq mc-passwd-timeout 600)
+
+      ;; To sign automatically every message you send, you need to add some
+      ;; lisp code. After adding the following lines to your .emacs file,
+      ;; (X)Emacs will ask you if the message is to be signed before sending
+      ;; it.
+
+      ;; (add-hook 'message-send-hook 'my-sign-message)
+      (defun my-sign-message ()
+        (if (yes-or-no-p "Sign message? ")
+            (mc-sign-message))))))
+
 
 ;;; packages.el ends here
