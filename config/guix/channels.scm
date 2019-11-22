84176d15876fa7901445c0e027e0193fb3f8a65b
Sharad Pratap <110339+sharad@users.noreply.github.com>
2019-11-22 14:03:29 +0530

    correction

---
+++ config/guix/channels.scm
@@ -0,0 +1,8 @@
+(cons* (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix"))
+
+       (channel
+        (name 'lotus)
+        (url "https://github.com/sharad/guix"))
+       %default-channels)
