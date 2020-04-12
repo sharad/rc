216ae69699550fa79fa8f9b16fe14a9217eedcbc
Sharad Pratap <110339+sharad@users.noreply.github.com>
2020-04-12 17:23:26 +0530

    correction

--- stumpwm/asdf.lisp
+++ stumpwm/asdf.lisp
@@ -6,12 +6,19 @@
 ;;{{
 (defun stumpwm-initialize-asdf ()
   (let* ((asdf-files '("/run/current-system/profile/share/sbcl/contrib/asdf/asdf.lisp"))
-         (asdf-files (member-if #'probe-file asdf-file))
+         (asdf-files (member-if #'probe-file asdf-files))
          (asdf-file  (car asdf-files)))
     (when asdf-file
+      (message "found asdf file ~a" asdf-file)
       (load asdf-file)
       (asdf:clear-source-registry)
-      (asdf:initialize-source-registry))))
+      (push #p"/run/current-system/profile/profile/lib/sbcl/" asdf:*central-registry*)
+      (push #p"/run/current-system/profile/profile/lib/sbcl/contrib/" asdf:*central-registry*)
+      (push #p"/home/s/hell/.guix-profile/lib/sbcl/" asdf:*central-registry*)
+      (push #p"/home/s/hell/.guix-profile/lib/sbcl/contrib/" asdf:*central-registry*)
+      ;; (asdf:initialize-source-registry)
+      )))
 
 (stumpwm-initialize-asdf)
+(asdf:initialize-source-registry)
 ;;}}
