4e74454f96e3e6f86247bfc829aad433e8daeee1
Sharad Pratap <110339+sharad@users.noreply.github.com>
2020-06-16 16:21:44 +0530

    correction

--- spacemacs-mycontribs/+local-session/lotus-editing/packages.el
+++ spacemacs-mycontribs/+local-session/lotus-editing/packages.el
@@ -79,164 +79,61 @@ Each entry is either:
 
 (defun lotus-editing/init-common-win ()
   (use-package common-win
-      :defer t
-      :config
-      (progn
-        (setq
-         x-select-enable-clipboard t
-         x-select-enable-primary t
-         ;; http://www.emacswiki.org/emacs/CopyAndPaste#toc5
-         select-active-regions 'only))))
+    :defer t
+    :init
+    (lotus-editing/init-common-win-init)
+    :config
+    (lotus-editing/init-common-win-config)))
 
 (defun lotus-editing/init-light-symbol ()
   (use-package light-symbol
-      :defer t
-      :config
-      (progn
-        ;; http://stackoverflow.com/a/385676/341107
-        ;; (add-element-to-lists 'light-symbol-mode pgm-langs)
-        ;; (light-symbol-mode 1) - not works
-        )))
+    :defer t
+    :init
+    (lotus-editing/init-light-symbol-init)
+    :config
+    (lotus-editing/init-light-symbol-config)))
 
 (defun lotus-editing/init-hilit-chg ()
   (use-package hilit-chg
     :defer t
+    :init
+    (lotus-editing/init-hilit-chg-init)
     :config
-    (progn
-      ;; (add-element-to-lists '(lambda ()
-      ;;                         (light-symbol-mode 1)
-      ;;                         (highlight-changes-visible-mode t)
-      ;;                         (highlight-changes-mode t)) pgm-langs)
-      ;; (highlight-changes-mode t) - not works
-      ;; (highlight-changes-visible-mode t)
-
-      ;;{{
-      ;; http://www.emacswiki.org/emacs/TrackChanges
-      (make-empty-face 'highlight-changes-saved-face)
-      (setq highlight-changes-face-list '(highlight-changes-saved-face))
-
-      ; Example: activate highlight changes with rotating faces for C programming
-      (add-hook 'c-mode-hook
-                (function (lambda ()
-                            (add-hook 'local-write-file-hooks 'highlight-changes-rotate-faces)
-                            (highlight-changes-mode t)
-                            ;; (... other stuff for setting up C mode ...)
-                            )))
-      ;;}}
-
-      ;;{{
-      (defun DE-highlight-changes-rotate-faces ()
-        (let ((toggle (eq highlight-changes-mode 'passive)))
-          (when toggle (highlight-changes-mode t))
-          (highlight-changes-rotate-faces)
-          (when toggle (highlight-changes-mode nil))))
-
-      ; Example for c-mode-hook:
-      (add-hook 'c-mode-hook
-                (function (lambda ()
-                            (add-hook 'local-write-file-hooks 'DE-highlight-changes-rotate-faces)
-                            (highlight-changes-mode t)
-                            (highlight-changes-mode nil)
-                            ;; (... other stuff for setting up C mode ...)
-                            )))
-      ;;}}
-
-      ;;{{
-      ;; Following function can make the highlight vanish after save file --coldnew
-      (defun highlight-changes-remove-after-save ()
-        "Remove previous changes after save."
-        (make-local-variable 'after-save-hook)
-        (add-hook 'after-save-hook
-                  (lambda ()
-                    (highlight-changes-remove-highlight (point-min) (point-max)))))
-      ;;}}
-      )))
+    (lotus-editing/init-hilit-chg-config)))
 
 (defun lotus-editing/init-highlight-symbol ()
   (use-package highlight-symbol
     :defer t
+    :init
+    (lotus-editing/init-highlight-symbol-init)
     :config
-    (progn)))
-      ;; (add-hook 'prog-mode-hook #'(lambda () (highlight-symbol-mode t)))
-      
+    (lotus-editing/init-highlight-symbol-config)))
 
-(defun lotus-editing/init-symbol-overlay ()
-  ;; https://stackoverflow.com/questions/385661/how-to-highlight-all-occurrences-of-a-word-in-an-emacs-buffer
-  (use-package symbol-overlay
-    :defer t
-    :config
-    (progn
-      (progn
-        (defun enable-symbol-overlay-mode ()
-          (unless (or (minibufferp)
-                      (derived-mode-p 'magit-mode)
-                      (derived-mode-p 'xref--xref-buffer-mode))
-            (symbol-overlay-mode t)))
-        (define-global-minor-mode global-symbol-overlay-mode ;; name of the new global mode
-          symbol-overlay-mode                                ;; name of the minor mode
-          enable-symbol-overlay-mode))
-      (progn
-        (global-symbol-overlay-mode)))))
+  (defun lotus-editing/init-symbol-overlay ()
+    ;; https://stackoverflow.com/questions/385661/how-to-highlight-all-occurrences-of-a-word-in-an-emacs-buffer
+    (use-package symbol-overlay
+      :defer t
+      :init
+      (lotus-editing/init-symbol-overlay-init)
+      :config
+      (lotus-editing/init-symbol-overlay-config)))
 
 (defun lotus-editing/init-show-wspace ()
   ;; http://emacswiki.org/emacs/ShowWhiteSpace
   (use-package show-wspace
     :defer t
+    :init
+    (lotus-editing/init-show-wspace-init)
     :config
-    (progn
-      )))
+    (lotus-editing/init-show-wspace-config)))
 
 (defun lotus-editing/init-paren ()
   (use-package paren
     :defer t
+    :init
+    (lotus-editing/init-paren-init)
     :config
-    (progn
-      (progn
-        ;; http://www.emacswiki.org/emacs/ShowParenMode#toc1
-        (setq
-         blink-matching-paren t
-         ;; blink-matching-paren is a variable defined in `simple.el'.
-         ;; Its value is jump
-
-         ;; Original value was t
-
-
-         ;; Documentation:
-         ;; Non-nil means show matching open-paren when close-paren is inserted.
-         ;; If t, highlight the paren.  If `jump', briefly move cursor to its
-         ;; position.  If `jump-offscreen', move cursor there even if the
-         ;; position is off screen.  With any other non-nil value, the
-         ;; off-screen position of the opening paren will be shown in the
-         ;; echo area.
-
-
-         )
-
-        (progn
-
-          (when nil
-            (progn
-              (progn                  ;NOT REQUIRED
-                ;; Hilight matching parenthesis
-                ;; (unless (featurep 'xemacs) (show-paren-mode 1))
-                (show-paren-mode 1)
-                ;; http://www.emacswiki.org/emacs/ShowParenMode#toc1
-                (defadvice show-paren-function (after show-matching-paren-offscreen activate)
-                  "If the matching paren is offscreen, show the matching line in the
-        echo area. Has no effect if the character before point is not of
-        the syntax class ')'."
-                  (interactive)
-                  (let* ((cb (char-before (point)))
-                         (matching-text (and cb
-                                             (char-equal (char-syntax cb) ?\))
-                                             (blink-matching-open))))
-                    (when (and
-                           matching-text
-                           (stringp matching-text))
-                      (message matching-text)))))
-              (progn
-                (when t
-                  (ad-remove-advice 'show-paren-function 'after 'show-matching-paren-offscreen))))))))))
+    (lotus-editing/init-paren-config)))
 
 (defun lotus-editing/init-corral ()
   ;; https://github.com/nivekuil/corral
@@ -249,58 +146,10 @@ Each entry is either:
 (defun lotus-editing/init-autorevert ()
   (use-package autorevert
     :defer t
+    :init
+    (lotus-editing/init-autorevert-init)
     :config
-    (progn
-      ;; TODO: write a new correct-auto-revert-notify-add-watch for emacs-snapshot
-      (when nil                         ;not workign with emacs-snapshot
-        (defun correct-auto-revert-notify-add-watch ()
-          "Enable file notification for current buffer's associated file."
-          ;; We can assume that `buffer-file-name' and
-          ;; `auto-revert-use-notify' are non-nil.
-          (if (or (string-match auto-revert-notify-exclude-dir-regexp
-                                (expand-file-name default-directory))
-                  (and
-                   buffer-file-name
-                   (file-symlink-p buffer-file-name)))
-              ;; Fallback to file checks.
-              (set (make-local-variable 'auto-revert-use-notify) nil)
-
-            (when (not auto-revert-notify-watch-descriptor)
-              (setq auto-revert-notify-watch-descriptor
-                    (ignore-errors
-                      (file-notify-add-watch
-                       (expand-file-name buffer-file-name default-directory)
-                       '(change attribute-change) 'auto-revert-notify-handler)))
-              (if auto-revert-notify-watch-descriptor
-                  (progn
-                    (puthash
-                     auto-revert-notify-watch-descriptor
-                     (cons (current-buffer)
-                           (gethash auto-revert-notify-watch-descriptor
-                                    auto-revert-notify-watch-descriptor-hash-list))
-                     auto-revert-notify-watch-descriptor-hash-list)
-                    (add-hook (make-local-variable 'kill-buffer-hook)
-                              'auto-revert-notify-rm-watch))
-                ;; Fallback to file checks.
-                (set (make-local-variable 'auto-revert-use-notify) nil)))))
-
-        (defalias 'auto-revert-notify-add-watch #'correct-auto-revert-notify-add-watch)
-
-        (defun auto-revert-notify-exclude-dir-regexp-reset-default ()
-          (setq auto-revert-notify-exclude-dir-regexp ;;default value
-                (concat
-                 ;; No mounted file systems.
-                 "^" (regexp-opt '("/afs/" "/media/" "/mnt" "/net/" "/tmp_mnt/"))
-                 ;; No remote files.
-                 (unless auto-revert-remote-files "\\|^/[^/|:][^/|]+:"))))
-
-        (defun auto-revert-notify-exclude-dir-regexp-add-regex (re)
-          (concat auto-revert-notify-exclude-dir-regexp re))
-
-        (setq
-         auto-revert-use-notify t       ;default
-         auto-revert-notify-exclude-dir-regexp (auto-revert-notify-exclude-dir-regexp-add-regex
-                                                (concat "\\|" "^" (expand-file-name "." "~") "/$")))))))
+    (lotus-editing/init-autorevert-config)))
 
 (defun lotus-editing/init-simple ()
   (use-package simple
@@ -313,43 +162,9 @@ Each entry is either:
 (defun lotus-editing/post-init-parinfer ()
   (use-package parinfer
     :defer t
+    :init
+    (lotus-editing/post-init-parinfer-init)
     :config
-    (progn
-
-      (progn
-        (when nil
-          ;; from delsel.el
-          (put 'self-insert-command 'delete-selection 'delete-selection-uses-region-p)
-
-          (put 'insert-char 'delete-selection t)
-          (put 'quoted-insert 'delete-selection t)
-
-          (put 'yank 'delete-selection 'yank)
-          (put 'clipboard-yank 'delete-selection 'yank)
-          (put 'insert-register 'delete-selection t)
-          ;; delete-backward-char and delete-forward-char already delete the selection by
-          ;; default, but not delete-char.
-          (put 'delete-char 'delete-selection 'supersede)
-
-          (put 'reindent-then-newline-and-indent 'delete-selection t)
-          (put 'newline-and-indent 'delete-selection t)
-          (put 'newline 'delete-selection t)
-          (put 'electric-newline-and-maybe-indent 'delete-selection t)
-          (put 'open-line 'delete-selection t)))
-
-      (progn
-        ;; (get 'parinfer-yank 'delete-selection)
-        ;; (get 'parinfer-smart-yank:yank 'delete-selection)
-        (put 'parinfer-yank 'delete-selection 'yank)
-        (put 'parinfer-smart-yank:yank 'delete-selection 'yank)))))
-
-
-
-(defun lotus-editing/init-PACKAGE ()
-  (use-package PACKAGE
-    :defer t
-    :config
-    (progn
-      )))
+    (lotus-editing/post-init-parinfer-config)))
 
 ;;; packages.el ends here
