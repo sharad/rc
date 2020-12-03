080f8d2fbc4e1ff6ae4289f67e2e27a6a78dc405
Sharad Pratap <110339+sharad@users.noreply.github.com>
2020-12-03 18:27:06 +0530

    correction

--- binx/keyring/get-imap-pass.py
+++ binx/keyring/get-imap-pass.py
@@ -1,48 +1,33 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
+import locale
+import subprocess
+import os
 import sys
 import getopt
-import gtk
-import gnomekeyring as gkey
 
-class Keyring(object):
-    def __init__(self, name, server, protocol):
-        self._name = name
-        self._server = server
-        self._protocol = protocol
-        self._keyring = gkey.get_default_keyring_sync()
+encoding = locale.getdefaultlocale()[1]
 
-    def has_credentials(self):
-        try:
-            attrs = {"server": self._server, "protocol": self._protocol}
-            items = gkey.find_items_sync(gkey.ITEM_NETWORK_PASSWORD, attrs)
-            return len(items) > 0
-        except gkey.DeniedError:
-            return False
+def get_password(server, protocol):
+    proc = subprocess.Popen(["secret-tool", "lookup", "server", server,  "protocol", protocol], stdout=subprocess.PIPE)
+    (out, err) = proc.communicate()
+    return out.decode(encoding).strip()
 
-    def get_credentials(self):
-        attrs = {"server": self._server, "protocol": self._protocol}
-        items = gkey.find_items_sync(gkey.ITEM_NETWORK_PASSWORD, attrs)
-        return (items[0].attributes["user"], items[0].secret)
-
-    def set_credentials(self, (user, pw)):
-        attrs = {
-                "user": user,
-                "server": self._server,
-                "protocol": self._protocol,
-            }
-        gkey.item_create_sync(gkey.get_default_keyring_sync(),
-                gkey.ITEM_NETWORK_PASSWORD, self._name, attrs, pw, True)
-
-def get_username(server):
-    keyring = Keyring("offlineimap", server, "imap")
-    (username, password) = keyring.get_credentials()
-    return username
-
-def get_password(server):
-    keyring = Keyring("offlineimap", server, "imap")
-    (username, password) = keyring.get_credentials()
-    return password
+def get_username(server, protocol):
+    if (sys.version_info[:3] < (3, 0)):
+        with open(os.devnull, 'wb') as devnull:
+            proc = subprocess.Popen(["secret-tool", "search", "server", server,  "protocol", protocol],
+                                    stdout=devnull,
+                                    stderr=subprocess.PIPE)
+            (out, err) = proc.communicate()
+            array = err.decode(encoding).strip().replace(" = ", ",").replace("\n", ",").split(",")
+            kvs = {array[i]: array[i + 1] for i in range(0, len(array), 2)}
+            return kvs["attribute.user"]
+    else:
+        err = subprocess.run(["secret-tool", "search", "server", server,  "protocol", protocol], stdout=subprocess.DEVNULL, stderr=subprocess.PIPE).stderr
+        array = err.decode(encoding).strip().replace(" = ", ",").replace("\n", ",").split(",")
+        kvs = {array[i]: array[i + 1] for i in range(0, len(array), 2)}
+        return kvs["attribute.user"]
 
 
 
@@ -54,21 +39,21 @@ def main(argv):
    try:
       opts, args = getopt.getopt(argv,"hpui:o:",["ifile=","ofile="])
    except getopt.GetoptError:
-      print 'get-imap-pass.py <server>'
+      print('get-imap-pass.py <server> <proto>')
       sys.exit(2)
 
    for opt, arg in opts:
       if opt == '-h':
-         print 'test.py -i <inputfile> -o <outputfile>'
+         print('test.py -i <inputfile> -o <outputfile>')
          sys.exit()
       elif opt in ("-i", "--ifile"):
          inputfile = arg
       elif opt in ("-o", "--ofile"):
          outputfile = arg
       elif opt == '-p':
-         print get_password(args[0])
+         print(get_password(args[0], "imap"))
       elif opt == '-u':
-         print get_username(args[0])
+         print(get_username(args[0], "imap"))
 
    # print args[0]
 
