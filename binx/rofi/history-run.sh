e541060b14bf031f882148d0a3c7f65984717f68
Sharad Pratap <110339+sharad@users.noreply.github.com>
2021-03-03 19:05:58 +0530

    corretion

--- binx/rofi/history-run.sh
+++ binx/rofi/history-run.sh
@@ -6,7 +6,7 @@ function main()
 {
     local _histfile="$1"
     local _sep=''
-    sed -e "s/${_sep}/${_sep}${_sep}/g" -e "s@^: [[:digit:]]\\+:0;@${_sep}@g" ${_histfile} | rofi -dmenu -sep ${_sep}
+    sed -e "s/${_sep}/${_sep}${_sep}/g" -e "s@^: [[:digit:]]\\+:0;@${_sep}@g" ${_histfile} | rofi -dmenu -sep ${_sep} -eh 2
 }
 
 
