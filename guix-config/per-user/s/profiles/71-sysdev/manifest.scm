fc07b8b8e086eb39f31a1a0420e77a6a3e87ca9f
Sharad Pratap <110339+sharad@users.noreply.github.com>
2021-04-15 18:24:07 +0530

    corretion

--- guix-config/per-user/s/profiles/71-sysdev/manifest.scm
+++ guix-config/per-user/s/profiles/71-sysdev/manifest.scm
@@ -2,14 +2,12 @@
 (define %lotus-sysdev-package-names
   (list "gparted" ;; required for situations
         "parted"
-        "gptdisk"
+        "gptfdisk"
         "efibootmgr"
         "grub-efi"))
 
 (define %lotus-sysdev-packages
-  (append
-   ;; (list '(gcc "lib"))
-   (map specification->package
-        %lotus-sysdev-package-names)))
+  (append (map specification->package
+               %lotus-sysdev-package-names)))
 
 (packages->manifest %lotus-sysdev-packages)
